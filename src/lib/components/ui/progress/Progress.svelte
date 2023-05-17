03f92796ad1b702f518f9259ee85d6199e5f3886
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-16 21:28:45 -0400

    progress

---
+++ src/lib/components/ui/progress/Progress.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Progress as ProgressPrimitive } from "radix-svelte";
+	import type { ProgressRootProps } from "radix-svelte";
+	import { cn } from "$lib/utils";
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+	export let value: ProgressRootProps["value"] = null;
+	export let max: ProgressRootProps["max"] = 100;
+
+	type $$Props = ProgressRootProps;
+</script>
+
+<ProgressPrimitive.Root
+	class={cn("relative h-4 w-full overflow-hidden rounded-full bg-secondary", className)}
+	{value}
+	{max}
+	{...$$restProps}
+>
+	<ProgressPrimitive.Indicator
+		class="h-full w-full flex-1 bg-primary transition-all"
+		style={`transform: translateX(-${100 - (100 * (value ?? 0)) / (max ?? 1)}%)`}
+	/>
+</ProgressPrimitive.Root>
---
+++ src/lib/stages/Progress.stage.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Stage } from "svelte-stage";
+	import StageBox from "./StageBox.svelte";
+	import { Progress } from "$components/ui/progress";
+</script>
+
+<Stage component="Progress">
+	<StageBox>
+		<Progress value={40} max={60} />
+	</StageBox>
+</Stage>
