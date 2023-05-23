19f58a3bd47cc5c9cd4da015e9ebacf3895d12b5
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-22 22:29:16 -0400

    initial codeblock wrapper functional

--- src/lib/components/docs/CodeBlockWrapper.svelte
+++ src/lib/components/docs/CodeBlockWrapper.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { buttonVariants } from "$components/ui/button";
-	import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "$components/ui/collapsible";
+	import { Collapsible, CollapsibleTrigger } from "$components/ui/collapsible";
 	import { cn } from "$lib/utils";
 
 	let className: string | undefined | null = undefined;
@@ -10,26 +10,21 @@
 </script>
 
 <Collapsible bind:open>
-	<div class={cn("relative overflow-hidden rounded-md border", className)} {...$$restProps}>
+	<div class={cn("relative overflow-hidden rounded-md mb-4", className)} {...$$restProps}>
+		<div class={cn("overflow-hidden h-full", !open && "max-h-32")}>
+			<slot />
+		</div>
 		<div
 			class={cn(
 				"absolute flex items-center justify-center bg-gradient-to-b from-background/30 to-muted/90 p-2",
 				open ? "inset-x-0 bottom-0 h-12" : "inset-0"
 			)}
 		>
-			<CollapsibleTrigger class={cn(buttonVariants({ variant: "secondary" }), "h-8 text-xs")}>
-				{open ? "Collapse" : expandButtonTitle}
-			</CollapsibleTrigger>
+			<span class={!open ? "pt-4" : ""}>
+				<CollapsibleTrigger class={cn(buttonVariants({ variant: "secondary" }), "h-8 text-xs")}>
+					{open ? "Collapse" : expandButtonTitle}
+				</CollapsibleTrigger>
+			</span>
 		</div>
-		<CollapsibleContent class={cn("overflow-hidden", !open && "max-h-32")}>
-			<div
-				class={cn(
-					"[&_pre]:my-0 [&_pre]:max-h-[650px] [&_pre]:pb-[100px]",
-					!open ? "[&_pre]:overflow-hidden" : "[&_pre]:overflow-auto]"
-				)}
-			>
-				<slot />
-			</div>
-		</CollapsibleContent>
 	</div>
 </Collapsible>
