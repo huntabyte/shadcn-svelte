300dc25938ef310441261e822672f830726e7235
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-26 12:40:54 -0400

    vercel adapter & workflow updates

--- apps/www/svelte.config.js
+++ apps/www/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import { mdsvex } from "mdsvex";
 import preprocess from "svelte-preprocess";
