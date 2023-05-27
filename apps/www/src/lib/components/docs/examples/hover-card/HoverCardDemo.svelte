785cc992fab798c297c3c4da2c954c5a7ca353d2
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-27 14:32:49 -0400

    chore(docs): copy on hovercard demo

--- apps/www/src/lib/components/docs/examples/hover-card/HoverCardDemo.svelte
+++ apps/www/src/lib/components/docs/examples/hover-card/HoverCardDemo.svelte
@@ -21,7 +21,7 @@
 			</Avatar>
 			<div class="space-y-1">
 				<h4 class="text-sm font-semibold">@sveltejs</h4>
-				<p class="text-sm">Web development, streamlined.</p>
+				<p class="text-sm">Cybernetically enhanced web apps.</p>
 				<div class="flex items-center pt-2">
 					<CalendarDays class="mr-2 h-4 w-4 opacity-70" />{" "}
 					<span class="text-xs text-muted-foreground">
