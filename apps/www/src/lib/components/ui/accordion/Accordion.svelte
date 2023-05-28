e9384d9bdd6f9eddc944d4047413acab02070ca6
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-28 00:12:20 -0400

    temp remove radix type due to issue

--- apps/www/src/lib/components/ui/accordion/Accordion.svelte
+++ apps/www/src/lib/components/ui/accordion/Accordion.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import type { AccordionRootProps } from "radix-svelte";
 	import { Accordion as AccordionPrimitive } from "radix-svelte";
 
 	// Remove the any when radix-svelte is updated to properly satisfy the types
 	// https://www.radix-svelte.com/docs/accordion
 	export let value: any = undefined;
-	type $$Props = AccordionRootProps;
 </script>
 
 <AccordionPrimitive.Root bind:value {...$$restProps}>
