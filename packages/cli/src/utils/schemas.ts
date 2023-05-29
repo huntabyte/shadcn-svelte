99a825b89ca59e574967237bbe582c0fcab4243a
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-05-29 13:45:17 -0400

    not needed anymore

--- packages/cli/src/utils/schemas.ts
+++ packages/cli/src/utils/schemas.ts
@@ -1,28 +1,7 @@
 import { z } from "zod";
 
-export const shadConfigSchema = z
-	.object({
-		value: z.object({
-			type: z.literal("ObjectExpression"),
-			properties: z.array(
-				z.object({
-					type: z.literal("Property"),
-					key: z.object({
-						type: z.literal("Identifier"),
-						name: z.enum(["componentPath"])
-					}),
-					value: z.object({
-						type: z.literal("Literal"),
-						value: z.string()
-					})
-				})
-			)
-		})
-	})
-	.transform((data) => {
-		return {
-			componentPath: data.value.properties[0].value.value
-		};
-	});
+export const shadConfigSchema = z.object({
+	componentPath: z.string()
+});
 
 export type ShadConfig = z.infer<typeof shadConfigSchema>;
