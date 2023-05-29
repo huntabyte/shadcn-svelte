1788f82fa253bba46220befa5ad42befed29ae96
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-05-29 10:46:40 -0400

    read the correct package json

--- packages/cli/src/utils/get-package-info.ts
+++ packages/cli/src/utils/get-package-info.ts
@@ -1,11 +1,18 @@
 // Credit to @shadcn for the original code. It has been slightly modified to fit the needs of this project.
 
 import path from "path";
+import { fileURLToPath } from "url";
 import fs from "fs-extra";
 import { type PackageJson } from "type-fest";
 
 export function getPackageInfo() {
-	const packageJsonPath = path.join("package.json");
+	const packageJsonPath = getPackageFilePath("../package.json");
 
 	return fs.readJSONSync(packageJsonPath) as PackageJson;
+}
+
+function getPackageFilePath(filePath: string) {
+	let distPath = fileURLToPath(new URL(`.`, import.meta.url));
+
+	return path.resolve(distPath, filePath);
 }
