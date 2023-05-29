1118a108dd25af133c9ff44b8848c2f78083a61c
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-28 23:04:56 -0400

    more metatags

--- apps/www/src/lib/components/docs/Metadata.svelte
+++ apps/www/src/lib/components/docs/Metadata.svelte
@@ -4,20 +4,13 @@
 
 	export let title: string = siteConfig.name;
 
-	$: title = $page.data?.title ? $page.data.title : siteConfig.name;
+	$: title = $page.data?.title
+		? `${$page.data.title} - ${siteConfig.name}`
+		: siteConfig.name;
 </script>
 
 <svelte:head>
-	{#if title === siteConfig.name}
-		<title>
-			{title}
-		</title>
-	{:else}
-		<title>
-			{title} - {siteConfig.name}
-		</title>
-	{/if}
-
+	<title>{title}</title>
 	<meta name="description" content={siteConfig.description} />
 	<meta name="keywords" content={siteConfig.keywords} />
 	<meta name="author" content="huntabyte" />
@@ -33,7 +26,11 @@
 	<meta property="og:url" content={siteConfig.url + $page.url.pathname} />
 	<meta property="og:image" content="https://shadcn-svelte.com/og.jpg" />
 	<meta property="og:image:alt" content={siteConfig.name} />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
 	<meta property="og:description" content={siteConfig.description} />
 	<meta property="og:site_name" content={siteConfig.name} />
 	<meta property="og:locale" content="EN_US" />
+	<link rel="shortcut icon" href="/favicon-16x16.png" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 </svelte:head>
