8b85408119482e500434d61e0b00cfde0bf1ad7f
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-31 19:27:09 -0400

    add prettier plugin svelte to eslint config

--- apps/www/.eslintrc.cjs
+++ apps/www/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
 		"prettier"
 	],
 	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint"],
+	plugins: ["@typescript-eslint", "prettier-plugin-svelte"],
 	parserOptions: {
 		sourceType: "module",
 		ecmaVersion: 2020,
