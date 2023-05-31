b809e4fdfa1bd4c59f0299a1a18da36cf67af5ba
Hunter Johnston <johnstonhuntera@gmail.com>
2023-05-31 19:30:26 -0400

    remove prettier plugin from eslint

--- apps/www/.eslintrc.cjs
+++ apps/www/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
 		"prettier"
 	],
 	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint", "prettier-plugin-svelte"],
+	plugins: ["@typescript-eslint"],
 	parserOptions: {
 		sourceType: "module",
 		ecmaVersion: 2020,
