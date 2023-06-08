f690b55daf332ae46e71988c0faf9f17baa80a4c
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-07 19:15:34 -0700

    is there a graceful way to handle pagination?

--- apps/www/src/lib/components/ui/select/Select.svelte
+++ apps/www/src/lib/components/ui/select/Select.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { createMenu } from "@grail-ui/svelte";
+	import { ChevronDown } from "lucide-svelte";
 	import { fade } from "svelte/transition";
 
 	type SelectItem = {
@@ -27,6 +28,18 @@
 		{
 			value: "pineapple",
 			label: "Pineapple"
+		},
+		{
+			value: "coconut",
+			label: "Coconut"
+		},
+		{
+			value: "lemon",
+			label: "Lemon"
+		},
+		{
+			value: "lime",
+			label: "Lime"
 		}
 	];
 
@@ -61,15 +74,21 @@
 		use:useTrigger
 		{...$triggerAttrs}
 		class="flex h-10 w-full items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-		>{selectedLabel ?? placeholder}</button
 	>
+		<span>
+			{selectedLabel ?? placeholder}
+		</span>
+		<span>
+			<ChevronDown class="h-4 w-4 opacity-50" />
+		</span>
+	</button>
 
 	{#if $open}
 		<ul
 			transition:fade
 			use:useMenu
 			{...$menuAttrs}
-			class="relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md animate-in fade-in-80 max-h-60 w-full"
+			class="relative z-50 min-w-[8rem] rounded-md border bg-popover text-popover-foreground shadow-md animate-in fade-in-80 max-h-60 w-full overflow-auto"
 		>
 			<li
 				class="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
--- apps/www/src/routes/examples/select/+page.svelte
+++ apps/www/src/routes/examples/select/+page.svelte
@@ -2,7 +2,7 @@
 	import { Select } from "$components/ui/select";
 </script>
 
-<div class="flex items-center justify-center w-full h-[500px]">
+<div class="flex items-center justify-center w-full h-[750px]">
 	<div class="w-full max-w-md">
 		<Select />
 	</div>
