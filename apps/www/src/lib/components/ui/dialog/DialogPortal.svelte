9a4ae876a35a9386599c4a76742a473b7477bb1d
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2023-06-08 19:38:14 -0400

    fix: remove unused props from dialog portal (#89)

--- apps/www/src/lib/components/ui/dialog/DialogPortal.svelte
+++ apps/www/src/lib/components/ui/dialog/DialogPortal.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
 	import { Dialog as DialogPrimitive } from "radix-svelte";
-	import { cn } from "$lib/utils";
-
-	let className: string | undefined | null = undefined;
-	export { className as class };
 </script>
 
-<DialogPrimitive.Portal class={cn(className)} {...$$restProps}>
+<DialogPrimitive.Portal>
 	<div
 		class="fixed inset-0 z-50 flex items-start justify-center sm:items-center"
 	>
