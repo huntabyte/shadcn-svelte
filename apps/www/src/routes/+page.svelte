a02c84646327b75be824da916c5dd4449963b080
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2023-06-10 13:44:04 -0400

    enhancement: add temp component roadmap announcement (#108)

--- apps/www/src/routes/+page.svelte
+++ apps/www/src/routes/+page.svelte
@@ -7,13 +7,25 @@
 		PageHeaderHeading
 	} from "$components/docs/page-header";
 	import { buttonVariants } from "$components/ui/button";
+	import { Separator } from "$components/ui/separator";
 	import { siteConfig } from "$lib/config/site";
 	import { cn } from "$lib/utils";
+	import { ChevronRight } from "lucide-svelte";
 </script>
 
 <div class="container relative pb-10">
 	<!-- <StyleSwitcher /> -->
 	<PageHeader>
+		<a
+			class="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
+			href="https://github.com/huntabyte/shadcn-svelte/issues/95"
+			target="_blank"
+			rel="noreferrer"
+		>
+			📢 <Separator class="mx-2 h-4" orientation="vertical" /> Temp Components
+			Roadmap Announcement
+			<ChevronRight class="ml-1 h-4 w-4" />
+		</a>
 		<PageHeaderHeading>Build your component library.</PageHeaderHeading>
 		<PageHeaderDescription>
 			Beautifully designed components that you can copy and paste into
