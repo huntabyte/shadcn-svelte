bcb3216260b45afa4b85158e8cd16c2b6a63a263
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-02 14:34:14 -0400

    refactor collapsible

---
+++ apps/www/src/lib/components/ui/collapsible/CollapsibleContent.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { slide } from "svelte/transition";
+	import { ctx, melt } from ".";
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+	const { content, open } = ctx.getContent();
+</script>
+
+{#if $open}
+	<div
+		use:melt={$content}
+		class={className}
+		{...$$restProps}
+		transition:slide
+	>
+		<slot />
+	</div>
+{/if}
