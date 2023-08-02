c3b2ea3a57fbf0d0bd188e4da277d381d1c40e95
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-02 16:14:19 -0400

    refactor label

--- apps/www/src/lib/components/ui/label/index.ts
+++ apps/www/src/lib/components/ui/label/index.ts
@@ -1,1 +1,10 @@
+import { createLabel, melt } from "@melt-ui/svelte";
+
 export { default as Label } from "./Label.svelte";
+export { melt };
+export function getLabel() {
+	const {
+		elements: { root: label }
+	} = createLabel();
+	return label;
+}
