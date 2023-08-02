5ac57c3620eab84b94fc51a304f8382edd6674e8
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-01 20:01:24 -0400

    upgrade deps

--- apps/www/svelte.config.js
+++ apps/www/svelte.config.js
@@ -1,17 +1,13 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import { mdsvex } from "mdsvex";
-import preprocess from "svelte-preprocess";
 import { mdsvexOptions } from "./mdsvex.config.js";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
+		mdsvex(mdsvexOptions),
 		vitePreprocess(),
-		preprocess({
-			postcss: true
-		}),
-		mdsvex(mdsvexOptions)
 	],
 
 	extensions: [".svelte", ".md"],
