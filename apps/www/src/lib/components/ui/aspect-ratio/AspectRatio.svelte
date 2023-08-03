43fb380f24d2e16a669e21d2574675b9935e1d8d
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-03 11:55:43 -0400

    refactor aspect ratio

--- apps/www/src/lib/components/ui/aspect-ratio/AspectRatio.svelte
+++ apps/www/src/lib/components/ui/aspect-ratio/AspectRatio.svelte
@@ -4,10 +4,22 @@
 	let className: string | undefined | null = undefined;
 	export { className as class };
 	export let ratio: number = 4 / 3;
-
-	$: ratioClass = `aspect-[${ratio}]`;
 </script>
 
-<div class={cn(ratioClass, className)} {...$$restProps}>
-	<slot />
+<div
+	style:position="relative"
+	style:width="100%"
+	style:padding-bottom="{100 / ratio}%"
+>
+	<div
+		style:position="absolute"
+		style:top="0"
+		style:right="0"
+		style:bottom="0"
+		style:left="0"
+		class={cn(className)}
+		{...$$restProps}
+	>
+		<slot />
+	</div>
 </div>
--- apps/www/src/lib/components/ui/combobox/index.ts
+++
@@ -1,6 +0,0 @@
-import {
-	melt,
-	createCombobox,
-	type CreateComboboxProps as ComboboxProps,
-	type Combobox as ComboboxReturn
-} from "@melt-ui/svelte";
