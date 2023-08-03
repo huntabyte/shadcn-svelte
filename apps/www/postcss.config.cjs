9a6f4d5522aaa90d48ba07cafa8a6f416c399188
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-08-03 10:37:59 -0400

    fix build

--- apps/www/postcss.config.cjs
+++ apps/www/postcss.config.cjs
@@ -5,7 +5,7 @@ const path = require("path");
 const config = {
 	plugins: [
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		tailwindcss(path.resolve(__dirname, "./tailwind.config.cjs")),
+		tailwindcss(path.resolve(__dirname, "./tailwind.config.js")),
 		//But others, like autoprefixer, need to run after,
 		autoprefixer
 	]
