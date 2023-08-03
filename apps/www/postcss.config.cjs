a5d856b32af5b7cab85e3a07590ef775b293c0f9
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-03 10:37:22 -0400

    fix build issues pt2

--- apps/www/postcss.config.cjs
+++ apps/www/postcss.config.cjs
@@ -1,7 +1,14 @@
-module.exports = {
-	plugins: {
-		"tailwindcss/nesting": {},
-		tailwindcss: {},
-		autoprefixer: {}
-	}
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const path = require("path");
+
+const config = {
+	plugins: [
+		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+		tailwindcss(path.resolve(__dirname, "./tailwind.config.js")),
+		//But others, like autoprefixer, need to run after,
+		autoprefixer
+	]
 };
+
+module.exports = config;
