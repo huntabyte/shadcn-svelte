4666a783631cdc1c109849c5c3e6c55504930588
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-03 22:47:17 -0400

    switch

--- apps/www/src/lib/components/ui/switch/index.ts
+++ apps/www/src/lib/components/ui/switch/index.ts
@@ -1,1 +1,20 @@
+import {
+	createSwitch,
+	melt,
+	type CreateSwitchProps as SwitchProps
+} from "@melt-ui/svelte";
+
+export { melt, SwitchProps };
 export { default as Switch } from "./Switch.svelte";
+
+export const ctx = {
+	get
+};
+
+function get(props: SwitchProps) {
+	const {
+		elements: { root },
+		states: { checked }
+	} = createSwitch(props);
+	return { root, isChecked: checked };
+}
