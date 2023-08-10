65ba9cb41e1130981797326cd30b737c57fbd47c
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-10 10:44:49 -0400

    checkbox update

--- apps/www/src/lib/registry/default/ui/checkbox/Checkbox.svelte
+++ apps/www/src/lib/registry/default/ui/checkbox/Checkbox.svelte
@@ -6,9 +6,11 @@
 	type $$Props = CheckboxPrimitive.Props;
 	let className: string | undefined | null = undefined;
 	export { className as class };
+	export let checked: $$Props["checked"] = false;
 </script>
 
 <CheckboxPrimitive.Root
+	bind:checked
 	class={cn(
 		"peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
 		className
