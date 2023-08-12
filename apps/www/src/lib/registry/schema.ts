4105787980cee4c8149b8e568d09bf73a2d94cb5
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-08-12 18:22:49 -0400

    added crawler

--- apps/www/src/lib/registry/schema.ts
+++ apps/www/src/lib/registry/schema.ts
@@ -3,8 +3,8 @@ import * as z from "zod";
 export const registrySchema = z.array(
 	z.object({
 		name: z.string(),
-		dependencies: z.array(z.string()).optional(),
-		registryDependencies: z.array(z.string()).optional(),
+		dependencies: z.array(z.string()),
+		registryDependencies: z.array(z.string()),
 		files: z.array(z.string()),
 		type: z.enum([
 			"components:ui",
