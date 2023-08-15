69891170a80b363b89e087c89d8af1752a79bd11
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-14 21:04:55 -0400

    default style initial form

---
+++ apps/www/src/lib/registry/default/ui/form/form-description.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { cn } from "@/utils";
+	import { getContext } from "svelte";
+	import type { FormFieldContext } from ".";
+	let className: string | undefined | null = undefined;
+	export { className as class };
+
+	const { formDescriptionId } = getContext<FormFieldContext>("FormField");
+</script>
+
+<p
+	class={cn("text-sm text-muted-foreground", className)}
+	{...$$restProps}
+	id={formDescriptionId}
+>
+	<slot />
+</p>
