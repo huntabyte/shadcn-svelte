11377b029152ffa60a90d9bf3b2256a6dbf0ef5d
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-18 20:54:39 -0400

    update meta tags

--- apps/www/src/lib/config/site.ts
+++ apps/www/src/lib/config/site.ts
@@ -1,16 +1,16 @@
 export const siteConfig = {
 	name: "shadcn-svelte",
 	url: "https://shadcn-svelte.com",
-	ogImage: "https://shadcn-svelte.com/og.jpg",
+	ogImage: "https://shadcn-svelte.com/og.png",
 	description:
-		"Beautifully designed components built with Radix Svelte and Tailwind CSS.",
+		"Beautifully designed components built with Melt UI and Tailwind CSS.",
 	links: {
 		twitter: "https://twitter.com/huntabyte",
 		github: "https://github.com/huntabyte/shadcn-svelte",
 		shadTwitter: "https://twitter.com/shadcn",
 		shadGithub: "https://github.com/shadcn/ui"
 	},
-	keywords: `shadcn,Svelte,SvelteKit,Svelte Components,TailwindCSS,Radix Svelte`
+	keywords: `shadcn,Svelte,SvelteKit,Svelte Components,TailwindCSS,Melt UI,Bit UI`
 };
 
 export type SiteConfig = typeof siteConfig;
