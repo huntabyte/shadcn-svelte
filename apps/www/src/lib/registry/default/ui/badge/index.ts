40764942c555ad1b92a0ce74436b3b6321ffd7e6
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-18 20:16:52 -0400

    prevent text selection on badge

--- apps/www/src/lib/registry/default/ui/badge/index.ts
+++ apps/www/src/lib/registry/default/ui/badge/index.ts
@@ -1,8 +1,8 @@
 import { tv, type VariantProps } from "tailwind-variants";
-
 export { default as Badge } from "./badge.svelte";
+
 export const badgeVariants = tv({
-	base: "inline-flex items-center border rounded-full px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+	base: "inline-flex items-center border rounded-full px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none select-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
 	variants: {
 		variant: {
 			default:
