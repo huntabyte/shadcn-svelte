ff4df5ca3d05c89a8df5f555bf588aede01fc904
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-18 20:18:27 -0400

    formatting

--- apps/www/src/routes/+page.svelte
+++ apps/www/src/routes/+page.svelte
@@ -16,14 +16,14 @@
 			class="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
 			href="/docs/installation"
 		>
-			🎉<Separator class="mx-2 h-4" orientation="vertical" /> New CLI, Styles, Themes,
-			& Components!
+			🎉<Separator class="mx-2 h-4" orientation="vertical" /> New CLI, Styles,
+			Themes, & Components!
 			<ChevronRight class="ml-1 h-4 w-4" />
 		</a>
 		<PageHeader.Heading>Build your component library.</PageHeader.Heading>
 		<PageHeader.Description>
-			Beautifully designed components that you can copy and paste into your
-			apps. Accessible. Customizable. Open Source.
+			Beautifully designed components that you can copy and paste into
+			your apps. Accessible. Customizable. Open Source.
 		</PageHeader.Description>
 		<p class="text-sm text-orange-700 dark:text-orange-400">
 			This is an unofficial port of <a
