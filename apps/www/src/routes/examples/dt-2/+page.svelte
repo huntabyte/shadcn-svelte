c80e7de2d969eec8a5875736173789485694fe84
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-24 13:57:30 -0400

    remove tmp dt

--- apps/www/src/routes/examples/dt-2/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import DataTable from "./data-table.svelte";
-</script>
-
-<DataTable />
--- apps/www/src/routes/examples/dt-2/data-table-checkbox.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { Checkbox } from "@/registry/new-york/ui/checkbox";
-	import type { Writable } from "svelte/store";
-
-	export let checked: Writable<boolean>;
-</script>
-
-<Checkbox bind:checked={$checked} />
