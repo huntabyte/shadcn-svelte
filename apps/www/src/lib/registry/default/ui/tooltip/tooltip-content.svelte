bb974f73a544fc6a9e858efc437c268b8cdfa0c5
Hunter Johnston <johnstonhuntera@gmail.com>
2023-08-25 18:53:04 -0400

    tooltip transition

--- apps/www/src/lib/registry/default/ui/tooltip/tooltip-content.svelte
+++ apps/www/src/lib/registry/default/ui/tooltip/tooltip-content.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Tooltip as TooltipPrimitive } from "@huntabyte/bits-ui";
-	import { cn } from "$lib/utils";
+	import { cn, transition } from "$lib/utils";
 
 	type $$Props = TooltipPrimitive.ContentProps;
 
@@ -10,9 +10,11 @@
 </script>
 
 <TooltipPrimitive.Content
+	{transition}
+	transitionConfig={{ y: 8, duration: 150 }}
 	{sideOffset}
 	class={cn(
-		"z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1",
+		"z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md",
 		className
 	)}
 	{...$$restProps}
