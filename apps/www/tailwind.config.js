4d6c56db68be04052bc708f0e611941bb84cc2d4
Konstantinos Kostarellis <delight@users.noreply.github.com>
2023-08-31 20:44:22 +0200

    chore: removes config to tailwindcss-animate in docs (#237)

--- apps/www/tailwind.config.js
+++ apps/www/tailwind.config.js
@@ -1,5 +1,4 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
-import tailwindcssAnimate from "tailwindcss-animate";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -58,8 +57,7 @@ const config = {
 				sans: ["Inter", ...fontFamily.sans]
 			}
 		}
-	},
-	plugins: [tailwindcssAnimate]
+	}
 };
 
 export default config;
