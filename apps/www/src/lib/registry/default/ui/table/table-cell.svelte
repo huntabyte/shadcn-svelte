6de52594bef57a255d6d6b0c482a2058d2cbf5ba
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2023-08-31 14:57:29 -0400

    fix: table cell and head types (#238)

--- apps/www/src/lib/registry/default/ui/table/table-cell.svelte
+++ apps/www/src/lib/registry/default/ui/table/table-cell.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { cn } from "$lib/utils";
-	import type { HTMLAttributes } from "svelte/elements";
+	import type { HTMLTdAttributes } from "svelte/elements";
 
-	type $$Props = HTMLAttributes<HTMLTableCellElement>;
+	type $$Props = HTMLTdAttributes;
 
 	let className: $$Props["class"] = undefined;
 	export { className as class };
