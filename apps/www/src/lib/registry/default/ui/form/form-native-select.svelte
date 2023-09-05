828189244add5a24cc2c2753a1ef5bcc9e78d56c
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-09-05 17:57:38 -0400

    fixed utils alias

--- apps/www/src/lib/registry/default/ui/form/form-native-select.svelte
+++ apps/www/src/lib/registry/default/ui/form/form-native-select.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Form as FormPrimitive } from "formsnap";
 	import { buttonVariants } from "@/registry/default/ui/button";
-	import { cn } from "@/utils";
+	import { cn } from "$lib/utils";
 	import { ChevronDown } from "lucide-svelte";
 	import type { HTMLSelectAttributes } from "svelte/elements";
 
--- apps/www/src/lib/registry/default/ui/form/form-validation.svelte
+++ apps/www/src/lib/registry/default/ui/form/form-validation.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Form as FormPrimitive } from "formsnap";
-	import { cn } from "@/utils";
+	import { cn } from "$lib/utils";
 	import type { HTMLAttributes } from "svelte/elements";
 
 	type $$Props = HTMLAttributes<HTMLParagraphElement>;
