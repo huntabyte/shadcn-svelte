3b9c9bdb5f812953bac6eee78dbafdcd62a5e7ec
HubbeDev <privat@responsivhub.com>
2023-09-08 20:04:58 +0200

    added whitespace-nowrap to button base

--- apps/www/src/lib/registry/new-york/ui/button/index.ts
+++ apps/www/src/lib/registry/new-york/ui/button/index.ts
@@ -3,7 +3,7 @@ import { tv, type VariantProps } from "tailwind-variants";
 import Root from "./button.svelte";
 
 const buttonVariants = tv({
-	base: "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+	base: "inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
 	variants: {
 		variant: {
 			default:
