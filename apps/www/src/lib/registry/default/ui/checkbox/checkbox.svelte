cbbea4f1551852773cb65e703a70b475fc3632ce
tmadge <tmadge@gmail.com>
2023-09-24 22:34:41 -0400

    Fix disabled checkbox styles (#307)

--- apps/www/src/lib/registry/default/ui/checkbox/checkbox.svelte
+++ apps/www/src/lib/registry/default/ui/checkbox/checkbox.svelte
@@ -12,11 +12,11 @@
 </script>
 
 <CheckboxPrimitive.Root
-	bind:checked
 	class={cn(
-		"peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+		"peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50",
 		className
 	)}
+	bind:checked
 	{...$$restProps}
 	on:click
 >
