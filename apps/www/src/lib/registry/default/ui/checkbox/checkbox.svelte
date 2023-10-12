7df93410e449d57fe901b5300c018b8fe12d2ae5
Walker Lockard <walker.b.lockard@gmail.com>
2023-10-11 23:44:12 -0500

    fix: check icon in checkbox is off-center (#330)

--- apps/www/src/lib/registry/default/ui/checkbox/checkbox.svelte
+++ apps/www/src/lib/registry/default/ui/checkbox/checkbox.svelte
@@ -13,7 +13,7 @@
 
 <CheckboxPrimitive.Root
 	class={cn(
-		"peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50",
+		"box-content peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50",
 		className
 	)}
 	bind:checked
@@ -26,9 +26,9 @@
 		let:isIndeterminate
 	>
 		{#if isChecked}
-			<Check class="h-4 w-4" />
+			<Check class="h-3.5 w-3.5" />
 		{:else if isIndeterminate}
-			<Minus class="h-4 w-4" />
+			<Minus class="h-3.5 w-3.5" />
 		{/if}
 	</CheckboxPrimitive.Indicator>
 </CheckboxPrimitive.Root>
