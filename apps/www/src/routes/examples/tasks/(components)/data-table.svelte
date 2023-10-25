36580f1153a6b9441e6e678a67ab974803e5f9a6
HubbeDev <privat@responsivhub.com>
2023-10-25 19:39:13 +0200

    Added pagnation to task table

--- apps/www/src/routes/examples/tasks/(components)/data-table.svelte
+++ apps/www/src/routes/examples/tasks/(components)/data-table.svelte
@@ -9,10 +9,19 @@
 	import type { Task } from "../(data)/schemas";
 	import {
 		addHiddenColumns,
+		addPagination,
 		addSelectedRows,
 		addSortBy,
 		addTableFilter
 	} from "svelte-headless-table/plugins";
+	import { Button } from "@/registry/default/ui/button";
+	import * as Select from "@/registry/default/ui/select";
+	import {
+		ChevronRight,
+		ChevronLeft,
+		DoubleArrowRight,
+		DoubleArrowLeft
+	} from "radix-icons-svelte";
 	import * as Table from "@/registry/new-york/ui/table";
 	import DataTableCheckbox from "./data-table-checkbox.svelte";
 	import DataTableTitleCell from "./data-table-title-cell.svelte";
@@ -29,6 +38,7 @@
 		sort: addSortBy({
 			toggleOrder: ["asc", "desc"]
 		}),
+		page: addPagination(),
 		filter: addTableFilter(),
 		hide: addHiddenColumns()
 	});
@@ -128,16 +138,27 @@
 		tableAttrs,
 		tableBodyAttrs,
 		flatColumns,
-		pluginStates
+		pluginStates,
+		rows
 	} = tableModel;
 
+	const { selectedDataIds } = pluginStates.select;
+
+	const { hasNextPage, hasPreviousPage, pageIndex, pageCount, pageSize } =
+		pluginStates.page;
+
 	const { hiddenColumnIds } = pluginStates.hide;
+
 	const ids = flatColumns.map((col) => col.id);
 	let hideForId = Object.fromEntries(ids.map((id) => [id, true]));
 
 	$: $hiddenColumnIds = Object.entries(hideForId)
 		.filter(([, hide]) => !hide)
 		.map(([id]) => id);
+
+	const updatePageSize = (selected) => {
+		$pageSize = selected.value;
+	};
 
 	const hidableCols = ["title", "status", "priority", "actions"];
 </script>
@@ -159,7 +180,10 @@
 								>
 									<Table.Head {...attrs}>
 										{#if cell.id !== "select" && cell.id !== "actions"}
-											<DataTableColumnHeader {props} title={cell.id} />
+											<DataTableColumnHeader
+												{props}
+												title={cell.id}
+											/>
 										{:else}
 											<Render of={cell.render()} />
 										{/if}
@@ -192,5 +216,77 @@
 				{/each}
 			</Table.Body>
 		</Table.Root>
+	</div>
+	<div class="flex items-center justify-between px-2">
+		<div class="flex-1 text-sm text-muted-foreground">
+			{Object.keys($selectedDataIds).length} of{" "}
+			{$rows.length} row(s) selected.
+		</div>
+		<div class="flex items-center space-x-6 lg:space-x-8">
+			<div class="flex items-center space-x-2">
+				<p class="text-sm font-medium">Rows per page</p>
+				<Select.Root
+					onSelectedChange={/** TODO: Fix type error Maby their is a better way to do this?? */
+					(selected) => updatePageSize(selected)}
+					selected={{ value: 10, label: "10" }}
+				>
+					<Select.Trigger class="w-[180px]">
+						<Select.Value placeholder="Select page size" />
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Item value="10">10</Select.Item>
+						<Select.Item value="20">20</Select.Item>
+						<Select.Item value="30">30</Select.Item>
+						<Select.Item value="40">40</Select.Item>
+						<Select.Item value="50">50</Select.Item>
+					</Select.Content>
+				</Select.Root>
+			</div>
+			<div
+				class="flex w-[100px] items-center justify-center text-sm font-medium"
+			>
+				Page {$pageIndex + 1} of {$pageCount}
+			</div>
+			<div class="flex items-center space-x-2">
+				<Button
+					variant="outline"
+					class="hidden h-8 w-8 p-0 lg:flex"
+					on:click={() => ($pageIndex = 0)}
+					disabled={!$hasPreviousPage}
+				>
+					<span class="sr-only">Go to first page</span>
+					<DoubleArrowLeft size={15} />
+				</Button>
+				<Button
+					variant="outline"
+					class="p-0 w-8 h-8"
+					on:click={() => ($pageIndex = $pageIndex - 1)}
+					disabled={!$hasPreviousPage}
+				>
+					<span class="sr-only">Go to previous page</span>
+					<ChevronLeft size={15} />
+				</Button>
+				<Button
+					variant="outline"
+					class="p-0 w-8 h-8"
+					disabled={!$hasNextPage}
+					on:click={() => ($pageIndex = $pageIndex + 1)}
+				>
+					<span class="sr-only">Go to next page</span>
+					<ChevronRight size={15} />
+				</Button>
+				<Button
+					variant="outline"
+					class="hidden h-8 w-8 p-0 lg:flex"
+					disabled={!$hasNextPage}
+					on:click={() =>
+						($pageIndex =
+							Math.ceil($rows.length / $pageRows.length) - 1)}
+				>
+					<span class="sr-only">Go to last page</span>
+					<DoubleArrowRight size={15} />
+				</Button>
+			</div>
+		</div>
 	</div>
 </div>
