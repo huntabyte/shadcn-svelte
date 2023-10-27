db6b9d6157c59ed767a99c60c4eec45da2aa27d2
Hunter Johnston <johnstonhuntera@gmail.com>
2023-10-27 00:25:57 -0400

    add combobox popover examples

--- apps/www/src/routes/sink/+page.svelte
+++ apps/www/src/routes/sink/+page.svelte
@@ -2,9 +2,13 @@
 	import { dev } from "$app/environment";
 	import { ComponentPreviewManual } from "$lib/components/docs";
 	import ComboboxDemo from "@/registry/new-york/example/combobox-demo.svelte";
+	import ComboboxPopover from "@/registry/new-york/example/combobox-popover.svelte";
 </script>
 
 {#if dev}
+	<ComponentPreviewManual>
+		<ComboboxPopover />
+	</ComponentPreviewManual>
 	<ComponentPreviewManual>
 		<ComboboxDemo />
 	</ComponentPreviewManual>
