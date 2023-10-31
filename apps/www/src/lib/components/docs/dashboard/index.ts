1dd73ef31faf26b066aa81346d3c990a9dee649e
Hunter Johnston <johnstonhuntera@gmail.com>
2023-10-31 15:29:17 -0400

    add teamswitcher to dashboard example

--- apps/www/src/lib/components/docs/dashboard/index.ts
+++ apps/www/src/lib/components/docs/dashboard/index.ts
@@ -4,3 +4,4 @@ export { default as Overview } from "./overview.svelte";
 export { default as RecentSales } from "./recent-sales.svelte";
 export { default as Search } from "./search.svelte";
 export { default as UserNav } from "./user-nav.svelte";
+export { default as TeamSwitcher } from "./team-switcher.svelte";
