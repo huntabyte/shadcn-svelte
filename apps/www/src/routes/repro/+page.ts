5e8475fb5314ccb77ead811163a4c2055f9e3251
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-02 15:26:25 -0400

    update repro redirect

--- apps/www/src/routes/repro/+page.ts
+++ apps/www/src/routes/repro/+page.ts
@@ -2,5 +2,8 @@ import { redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async () => {
-	throw redirect(302, "https://stackblitz.com/edit/shadcn-svelte");
+	throw redirect(
+		302,
+		"https://stackblitz.com/github/huntabyte/shadcn-repro-template"
+	);
 };
