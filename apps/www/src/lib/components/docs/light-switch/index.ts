9760a7a6e178bbfae71ea4d5dc2215d5e28b48f2
Hunter Johnston <johnstonhuntera@gmail.com>
2023-11-07 22:21:34 -0500

    add doc search & modernize docs

--- apps/www/src/lib/components/docs/light-switch/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as LightSwitch } from "./light-switch.svelte";
---
+++ apps/www/src/lib/components/docs/mode-toggle.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Moon, Sun } from "radix-icons-svelte";
+	import { Button } from "@/registry/new-york/ui/button";
+	import * as DropdownMenu from "@/registry/new-york/ui/dropdown-menu";
+	import { resetMode, setMode } from "mode-watcher";
+</script>
+
+<DropdownMenu.Root positioning={{ placement: "bottom-end" }}>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button builders={[builder]} variant="ghost" class="w-9 px-0">
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-roate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		<DropdownMenu.Item on:click={() => setMode("light")}>
+			Light
+		</DropdownMenu.Item>
+		<DropdownMenu.Item on:click={() => setMode("dark")}>
+			Dark
+		</DropdownMenu.Item>
+		<DropdownMenu.Item on:click={() => resetMode()}>
+			System
+		</DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
