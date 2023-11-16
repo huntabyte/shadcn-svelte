147361b00016319d23c325a81aa3d392667408b2
Fernando López Guevara <fernando.lguevara@gmail.com>
2023-11-16 15:15:40 -0300

    remove select demo component

--- apps/www/src/lib/registry/default/example/popover-demo.svelte
+++ apps/www/src/lib/registry/default/example/popover-demo.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import {
-		Popover,
-		PopoverTrigger,
-		PopoverContent
-	} from "@/registry/default/ui/popover";
-	import { Label } from "@/registry/default/ui/label";
 	import { Button } from "@/registry/default/ui/button";
 	import { Input } from "@/registry/default/ui/input";
-	import SelectDemo from "./select-demo.svelte";
+	import { Label } from "@/registry/default/ui/label";
+	import {
+		Popover,
+		PopoverContent,
+		PopoverTrigger
+	} from "@/registry/default/ui/popover";
 </script>
 
 <Popover portal={null}>
@@ -41,6 +40,5 @@
 				</div>
 			</div>
 		</div>
-		<SelectDemo />
 	</PopoverContent>
 </Popover>
