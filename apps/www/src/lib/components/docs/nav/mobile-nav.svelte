72d54afe89a1510a63cb059545555d7366c10adb
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-11-16 19:50:29 +0000

    fix: remove unused classes

--- apps/www/src/lib/components/docs/nav/mobile-nav.svelte
+++ apps/www/src/lib/components/docs/nav/mobile-nav.svelte
@@ -51,7 +51,7 @@
 										{item.title}
 										{#if item.label}
 											<span
-												class="ml-2 rounded-md bg-[#adfa1d] px-1.5 py-0.5 text-xs leading-none text-[#000000] no-underline group-hover:no-underline"
+												class="ml-2 rounded-md bg-[#adfa1d] px-1.5 py-0.5 text-xs leading-none text-[#000000]"
 											>
 												{item.label}
 											</span>
