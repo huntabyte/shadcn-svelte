5dda1c521fe6991b8c8e85d79839bd8d9e3a7330
enchart <77974051+enchart@users.noreply.github.com>
2023-12-08 00:28:51 +0500

    fix: make `DropdownMenu.SubTrigger` use accent foreground color

--- apps/www/src/lib/registry/new-york/ui/dropdown-menu/dropdown-menu-sub-trigger.svelte
+++ apps/www/src/lib/registry/new-york/ui/dropdown-menu/dropdown-menu-sub-trigger.svelte
@@ -15,7 +15,7 @@
 
 <DropdownMenuPrimitive.SubTrigger
 	class={cn(
-		"flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[highlighted]:bg-accent data-[state=open]:bg-accent",
+		"flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[highlighted]:bg-accent data-[state=open]:bg-accent data-[highlighted]:text-accent-foreground data-[state=open]:text-accent-foreground",
 		inset && "pl-8",
 		className
 	)}
