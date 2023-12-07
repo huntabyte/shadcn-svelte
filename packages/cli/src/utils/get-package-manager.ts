d9cc40c8388d32b3f9bb845caa11d19840674a67
CokaKoala <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-07 12:03:06 -0500

    [CLI] fix: Use user's process's PM to install deps if no lockfile (#509)

--- packages/cli/src/utils/get-package-manager.ts
+++ packages/cli/src/utils/get-package-manager.ts
@@ -4,10 +4,18 @@ export async function getPackageManager(
 	targetDir: string
 ): Promise<"yarn" | "pnpm" | "bun" | "npm"> {
 	const packageManager = await detect({ programmatic: true, cwd: targetDir });
-
 	if (packageManager === "yarn@berry") return "yarn";
 	if (packageManager === "pnpm@6") return "pnpm";
-	if (packageManager === "bun") return "bun";
 
+	// ni couldn't find a lockfile, so we'll try to determine the PM via the user agent
+	if (packageManager === null) {
+		const userAgent = process.env.npm_config_user_agent;
+
+		if (userAgent === undefined) return "npm";
+		if (userAgent.startsWith("yarn")) return "yarn";
+		if (userAgent.startsWith("pnpm")) return "pnpm";
+	}
+
+	// default to npm as the last resort
 	return packageManager ?? "npm";
 }
