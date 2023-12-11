7d541ed0704fcf0c5b64906d1bded41991ea2683
Olle Månsson <31876997+ollema@users.noreply.github.com>
2023-12-11 15:46:27 +0100

    chore: update mode-watcher & add fallback mode before stores load (#526)

--- apps/www/src/lib/components/docs/theme-customizer/customizer.svelte
+++ apps/www/src/lib/components/docs/theme-customizer/customizer.svelte
@@ -121,7 +121,9 @@
 							"justify-start",
 							isActive && "border-2 border-primary"
 						)}
-						style="--theme-primary: hsl({theme?.activeColor[$mode]}"
+						style="--theme-primary: hsl({theme?.activeColor[
+							$mode ?? 'dark'
+						]}"
 					>
 						<span
 							class="mr-1 flex h-5 w-5 shrink-0 -translate-x-1 items-center justify-center rounded-full bg-[--theme-primary]"
