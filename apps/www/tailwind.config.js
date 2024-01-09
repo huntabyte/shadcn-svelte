5987e2a1c92ac8cf87bf990164802b8eca4045bb
hubbeDev <privat@responsivhub.com>
2024-01-10 00:33:57 +0100

    Replace inter with Geist font

--- apps/www/tailwind.config.js
+++ apps/www/tailwind.config.js
@@ -57,7 +57,7 @@ const config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: ["Inter", ...fontFamily.sans]
+				sans: ["var(--font-geist-sans)", ...fontFamily.sans]
 			}
 		}
 	}
