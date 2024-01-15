a2e5fc91c26a43152400606ae1c31cf172500f24
Hunter Johnston <johnstonhuntera@gmail.com>
2024-01-15 09:43:00 -0800

    Update carousel next and previous buttons styling

--- apps/www/src/lib/registry/new-york/ui/carousel/carousel-next.svelte
+++ apps/www/src/lib/registry/new-york/ui/carousel/carousel-next.svelte
@@ -20,7 +20,7 @@
 	{variant}
 	{size}
 	class={cn(
-		"absolute h-8 w-8 rounded-full",
+		"absolute h-8 w-8 rounded-full touch-manipulation",
 		$orientation === "horizontal"
 			? "-right-12 top-1/2 -translate-y-1/2"
 			: "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
