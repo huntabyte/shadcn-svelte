382235b113809f1a462f1d6a8d0aa0250ec56af3
hubbeDev <privat@responsivhub.com>
2024-01-25 19:22:11 +0100

    Replace min width of menu subcontent with min-w-max

--- apps/www/src/lib/registry/new-york/ui/menubar/menubar-sub-content.svelte
+++ apps/www/src/lib/registry/new-york/ui/menubar/menubar-sub-content.svelte
@@ -16,7 +16,7 @@
 	{transition}
 	{transitionConfig}
 	class={cn(
-		"z-50 min-w-[8rem] rounded-md border bg-popover p-1 text-popover-foreground shadow-lg focus:outline-none",
+		"z-50 min-w-max rounded-md border bg-popover p-1 text-popover-foreground shadow-lg focus:outline-none",
 		className
 	)}
 	{...$$restProps}
