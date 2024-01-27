7a948ea43a093952cd0169e9d46ea21ffaab792c
Scooter1337 <lucav007@hotmail.com>
2024-01-27 02:06:31 +0100

    More style parity with shadcn-react.

--- apps/www/src/lib/registry/new-york/ui/button/index.ts
+++ apps/www/src/lib/registry/new-york/ui/button/index.ts
@@ -10,7 +10,7 @@ const buttonVariants = tv({
 			destructive:
 				"bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
 			outline:
-				"border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
+				"border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
 			secondary: "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
 			ghost: "hover:bg-accent hover:text-accent-foreground",
 			link: "text-primary underline-offset-4 hover:underline"
