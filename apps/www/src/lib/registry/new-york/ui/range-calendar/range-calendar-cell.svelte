a93d3976fd7c4f866ddf407a5b4bc92b6e740f45
Anatol Zakrividoroga <53095479+anatolzak@users.noreply.github.com>
2024-01-29 22:24:02 +0200

    fix: calendar start selection left border radius (#712)

--- apps/www/src/lib/registry/new-york/ui/range-calendar/range-calendar-cell.svelte
+++ apps/www/src/lib/registry/new-york/ui/range-calendar/range-calendar-cell.svelte
@@ -12,7 +12,7 @@
 <RangeCalendarPrimitive.Cell
 	{date}
 	class={cn(
-		"text-center text-sm p-0 relative [&:has([data-selected][data-selection-end])]:rounded-r-md [&:has([data-selected][data-outside-month])]:bg-accent/50 [&:has([data-selected])]:bg-accent first:[&:has([data-selected])]:rounded-l-md last:[&:has([data-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+		"text-center text-sm p-0 relative [&:has([data-selected][data-selection-start])]:rounded-l-md [&:has([data-selected][data-selection-end])]:rounded-r-md [&:has([data-selected][data-outside-month])]:bg-accent/50 [&:has([data-selected])]:bg-accent first:[&:has([data-selected])]:rounded-l-md last:[&:has([data-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
 		className
 	)}
 	{...$$restProps}
