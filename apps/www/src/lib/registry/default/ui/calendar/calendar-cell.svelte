a64f1cb0d58803bcb031b572793e808914a1faec
Anatol Zakrividoroga <53095479+anatolzak@users.noreply.github.com>
2024-02-06 18:46:23 +0100

    fix: calendar selected day border radius default theme

--- apps/www/src/lib/registry/default/ui/calendar/calendar-cell.svelte
+++ apps/www/src/lib/registry/default/ui/calendar/calendar-cell.svelte
@@ -12,7 +12,7 @@
 <CalendarPrimitive.Cell
 	{date}
 	class={cn(
-		"h-9 w-9 text-center text-sm p-0 relative [&:has([data-selected][data-outside-month])]:bg-accent/50 [&:has([data-selected])]:bg-accent first:[&:has([data-selected])]:rounded-l-md last:[&:has([data-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+		"h-9 w-9 text-center text-sm p-0 relative [&:has([data-selected][data-outside-month])]:bg-accent/50 [&:has([data-selected])]:bg-accent [&:has([data-selected])]:rounded-md focus-within:relative focus-within:z-20",
 		className
 	)}
 	{...$$restProps}
