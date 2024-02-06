45c5eeb0c553c60f8fc05783752f9a3a7c40eeff
Hichem Fantar <hichemfantar2049@gmail.com>
2024-02-07 00:54:15 +0100

    Add better transitions for radio group, slider, copy button, textarea, button, select trigger, and input

--- apps/www/src/lib/components/docs/copy-button.svelte
+++ apps/www/src/lib/components/docs/copy-button.svelte
@@ -100,7 +100,7 @@
 {:else}
 	<button
 		class={cn(
-			"inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 z-10 h-6 w-6 text-zinc-50 hover:bg-zinc-700 hover:text-zinc-50 absolute right-4 top-4",
+			"inline-flex items-center justify-center rounded-md text-sm font-medium transition focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 z-10 h-6 w-6 text-zinc-50 hover:bg-zinc-700 hover:text-zinc-50 absolute right-4 top-4",
 			className
 		)}
 		use:clickToCopyAction={value}
--- apps/www/src/lib/registry/default/ui/slider/slider.svelte
+++ apps/www/src/lib/registry/default/ui/slider/slider.svelte
@@ -21,7 +21,7 @@
 	{#each thumbs as thumb}
 		<SliderPrimitive.Thumb
 			{thumb}
-			class="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+			class="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
 		/>
 	{/each}
 </SliderPrimitive.Root>
