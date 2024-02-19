741eb1773f68367a41bc2bf853a3c79a9ea87fe5
Hunter Johnston <johnstonhuntera@gmail.com>
2024-02-19 12:11:03 -0500

    more demos

--- apps/www/src/lib/registry/default/example/form-demo.svelte
+++ apps/www/src/lib/registry/default/example/form-demo.svelte
@@ -9,28 +9,28 @@
 
 <script lang="ts">
 	import * as Form from "@/registry/default/ui/form";
-	import type { SuperValidated, Infer } from "sveltekit-superforms";
-	import { toast } from "svelte-sonner";
+	import { Input } from "@/registry/default/ui/input";
+	import { type SuperValidated, type Infer, superForm } from "sveltekit-superforms";
+	import { zodClient } from "sveltekit-superforms/adapters";
 
-	export let form: SuperValidated<Infer<FormSchema>>;
+	let data: SuperValidated<Infer<FormSchema>>;
+	export { data as form };
+	const form = superForm(data, {
+		validators: zodClient(formSchema),
+	});
+
+	const { form: formData, enhance } = form;
 </script>
 
-<!-- <Form.Root
-	schema={formSchema}
-	{form}
-	{options}
-	let:config
-	method="POST"
-	action="?/username"
-	class="w-2/3 space-y-6"
->
-	<Form.Field {config} name="username">
-		<Form.Item>
+<form action="?/username" method="POST" class="w-2/3 space-y-6" use:enhance>
+	<Form.Field {form} name="username">
+		<Form.Control let:attrs>
 			<Form.Label>Username</Form.Label>
-			<Form.Input />
-			<Form.Description>This is your public display name.</Form.Description>
-			<Form.Validation />
-		</Form.Item>
+			<Input {...attrs} bind:value={$formData.username} />
+		</Form.Control>
+		<Form.Description>This is your public display name.</Form.Description>
+		<Form.FieldErrors />
 	</Form.Field>
+
 	<Form.Button>Submit</Form.Button>
-</Form.Root> -->
+</form>
