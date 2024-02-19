6868328eccaff1f9532e344314c740dc37c16aa3
Hunter Johnston <johnstonhuntera@gmail.com>
2024-02-18 21:20:18 -0500

    formatting

--- apps/www/src/routes/+layout.svelte
+++ apps/www/src/routes/+layout.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
 	import { page } from "$app/stores";
-	import { dev, browser } from "$app/environment";
+	import { dev } from "$app/environment";
 	import { Metadata, SiteFooter, SiteHeader, TailwindIndicator } from "$components/docs";
 	import { updateTheme } from "@/utils";
 	import "../styles/globals.css";
 	import { config } from "@/stores";
-	import * as Fathom from "fathom-client";
 	import { ModeWatcher } from "mode-watcher";
 	import { Toaster as DefaultSonner } from "@/registry/default/ui/sonner";
 	import { Toaster as NYSonner } from "@/registry/new-york/ui/sonner";
 
 	$: updateTheme($config.theme, $page.url.pathname);
-	$: $page.url.pathname, browser && Fathom.trackPageview();
 </script>
 
 <ModeWatcher />
