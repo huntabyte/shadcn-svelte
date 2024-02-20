46fad3f18041b8a9e25de5c6ca2d3099f1baeae4
Hunter Johnston <johnstonhuntera@gmail.com>
2024-02-19 23:01:26 -0500

    formatting

--- apps/www/src/lib/registry/default/ui/select/select-item.svelte
+++ apps/www/src/lib/registry/default/ui/select/select-item.svelte
@@ -18,7 +18,7 @@
 	{disabled}
 	{label}
 	class={cn(
-		"data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+		"relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50",
 		className
 	)}
 	{...$$restProps}
