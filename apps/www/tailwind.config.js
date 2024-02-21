88bfd28ca84385ff293a22b6b79301bc51574497
Hunter Johnston <johnstonhuntera@gmail.com>
2024-02-20 21:13:29 -0500

    test

--- apps/www/tailwind.config.js
+++ apps/www/tailwind.config.js
@@ -55,7 +55,7 @@ const config = {
 				sm: "calc(var(--radius) - 4px)",
 			},
 			fontFamily: {
-				sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+				sans: ["geist-sans", ...fontFamily.sans],
 			},
 		},
 	},
