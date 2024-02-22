dd99e05ae3a951f029c8ed5c7b80af99498c4d6d
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-02-22 03:07:17 +0200

    docs: improve metric cards

--- apps/www/src/lib/components/docs/charts/metric.svelte
+++ apps/www/src/lib/components/docs/charts/metric.svelte
@@ -1,49 +1,47 @@
 <script lang="ts">
-	import { VisXYContainer, VisLine } from "@unovis/svelte";
-	import { colorArray } from "./helpers";
+	import { VisXYContainer, VisLine, VisTooltip, VisScatter, VisCrosshair } from "@unovis/svelte";
+	import {
+		lineColors,
+		type Data,
+		scatterPointColors,
+		scatterPointStrokeColors,
+		tooltipTemplate,
+		crosshairPointColors,
+		crosshairStrokeWidths,
+	} from "./helpers";
 
-	type Data = { average: number; today: number; id: number };
 	const data: Data[] = [
-		{
-			id: 1,
-			average: 400,
-			today: 240,
-		},
-		{
-			id: 2,
-			average: 300,
-			today: 139,
-		},
-		{
-			id: 3,
-			average: 200,
-			today: 980,
-		},
-		{
-			id: 4,
-			average: 278,
-			today: 390,
-		},
-		{
-			id: 5,
-			average: 189,
-			today: 480,
-		},
-		{
-			id: 6,
-			average: 239,
-			today: 380,
-		},
-		{
-			id: 7,
-			average: 349,
-			today: 430,
-		},
+		{ id: 1, average: 400, today: 240 },
+		{ id: 2, average: 300, today: 139 },
+		{ id: 3, average: 200, today: 980 },
+		{ id: 4, average: 278, today: 390 },
+		{ id: 5, average: 189, today: 480 },
+		{ id: 6, average: 239, today: 380 },
+		{ id: 7, average: 349, today: 430 },
 	];
+
 	const x = (d: Data) => d.id;
 	const y = [(d: Data) => d.today, (d: Data) => d.average];
 </script>
 
-<VisXYContainer {data} height={200}>
-	<VisLine {x} {y} color={colorArray()} />
+<VisXYContainer {data} height={200} margin={{ top: 5, right: 10, left: 10, bottom: 0 }}>
+	<VisTooltip />
+	<VisLine {x} {y} lineWidth={1} color={lineColors} />
+	<VisScatter
+		{x}
+		{y}
+		color={scatterPointColors}
+		size={6}
+		strokeColor={scatterPointStrokeColors}
+		strokeWidth={crosshairStrokeWidths}
+	/>
+	<VisCrosshair template={tooltipTemplate} color={crosshairPointColors} />
 </VisXYContainer>
+
+<style>
+	:root {
+		--vis-tooltip-padding: "0px";
+		--vis-tooltip-background-color: "transparent";
+		--vis-tooltip-border-color: "transparent";
+	}
+</style>
--- apps/www/src/lib/components/docs/charts/revenue.svelte
+++ apps/www/src/lib/components/docs/charts/revenue.svelte
@@ -1,40 +1,16 @@
 <script lang="ts">
-	import { VisXYContainer, VisLine } from "@unovis/svelte";
-	import { color } from "./helpers";
+	import { VisXYContainer, VisLine, VisScatter } from "@unovis/svelte";
+	import { color, scatterPointColors, scatterPointStrokeColors } from "./helpers";
 
 	const data = [
-		{
-			id: 1,
-			revenue: 10400,
-		},
-		{
-			id: 2,
-			revenue: 14405,
-		},
-		{
-			id: 3,
-			revenue: 9400,
-		},
-		{
-			id: 4,
-			revenue: 8200,
-		},
-		{
-			id: 5,
-			revenue: 7000,
-		},
-		{
-			id: 6,
-			revenue: 9600,
-		},
-		{
-			id: 7,
-			revenue: 11244,
-		},
-		{
-			id: 8,
-			revenue: 26475,
-		},
+		{ id: 1, revenue: 10400 },
+		{ id: 2, revenue: 14405 },
+		{ id: 3, revenue: 9400 },
+		{ id: 4, revenue: 8200 },
+		{ id: 5, revenue: 7000 },
+		{ id: 6, revenue: 9600 },
+		{ id: 7, revenue: 11244 },
+		{ id: 8, revenue: 26475 },
 	];
 	const x = (d: { revenue: number; id: number }) => d.id;
 	const y = (d: { revenue: number; id: number }) => d.revenue;
@@ -42,4 +18,12 @@
 
 <VisXYContainer {data} height="80">
 	<VisLine {x} {y} color={color()} />
+	<VisScatter
+		{x}
+		{y}
+		size={6}
+		color={scatterPointColors}
+		strokeColor={scatterPointStrokeColors}
+		strokeWidth={2}
+	/>
 </VisXYContainer>
