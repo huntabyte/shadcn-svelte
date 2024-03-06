dd61235d97b1549c556a0971ebe47ca6ecec765d
Hunter Johnston <johnstonhuntera@gmail.com>
2024-03-06 11:02:34 -0500

    fix form button

--- apps/www/src/lib/registry/new-york/ui/form/form-button.svelte
+++ apps/www/src/lib/registry/new-york/ui/form/form-button.svelte
@@ -2,8 +2,9 @@
 	import * as Button from "@/registry/new-york/ui/button/index.js";
 
 	type $$Props = Button.Props;
+	type $$Events = Button.Events;
 </script>
 
-<Button.Root type="submit" {...$$restProps}>
+<Button.Root type="submit" on:click on:keydown {...$$restProps}>
 	<slot />
 </Button.Root>
