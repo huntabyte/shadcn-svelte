9208aa150e1123b7e3450b78e03968a2aece10a8
ekpah <49120297+ekpah@users.noreply.github.com>
2024-03-07 11:24:50 +0100

    Update scroll-area-scrollbar.svelte
    change utils.js to utils in Scroll-Area-Scrollbar

--- apps/www/src/lib/registry/default/ui/scroll-area/scroll-area-scrollbar.svelte
+++ apps/www/src/lib/registry/default/ui/scroll-area/scroll-area-scrollbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { ScrollArea as ScrollAreaPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils.js";
+	import { cn } from "$lib/utils";
 
 	type $$Props = ScrollAreaPrimitive.ScrollbarProps & {
 		orientation?: "vertical" | "horizontal";
