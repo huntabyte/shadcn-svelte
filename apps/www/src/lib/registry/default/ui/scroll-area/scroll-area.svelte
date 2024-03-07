8079891ba659e54cd1e214ff4f41738eee0163da
ekpah <49120297+ekpah@users.noreply.github.com>
2024-03-07 11:30:02 +0100

    Update scroll-area.svelte

--- apps/www/src/lib/registry/default/ui/scroll-area/scroll-area.svelte
+++ apps/www/src/lib/registry/default/ui/scroll-area/scroll-area.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { ScrollArea as ScrollAreaPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils.js";
+	import { cn } from "$lib/utils";
 	import { Scrollbar } from "./index.js";
 
 	type $$Props = ScrollAreaPrimitive.Props & {
