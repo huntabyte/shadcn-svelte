4f054651f0b4f74ce2cd525c8752651d023b504e
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-03-09 02:47:16 -0500

    further simplify

--- apps/www/src/routes/docs/+page.ts
+++ apps/www/src/routes/docs/+page.ts
@@ -1,22 +1,10 @@
 import { error } from "@sveltejs/kit";
-import type { DocResolver } from "$lib/types/docs.js";
+import type { DocFile } from "$lib/types/docs.js";
 import type { PageLoad } from "./$types.js";
 
 export const load: PageLoad = async () => {
-	// there's definitely a better way to do this for the index page
-	// but I'll sort this out later - works for now :)
-	const modules = import.meta.glob(`/src/content/**/index.md`);
-
-	let resolver;
-
-	for (const [path, res] of Object.entries(modules)) {
-		if (path === "/src/content/index.md") {
-			resolver = res as DocResolver;
-			break;
-		}
-	}
-
-	const doc = await resolver?.();
+	// @ts-expect-error import doesn't recognize *.md modules _yet_
+	const doc: DocFile = await import("../../content/index.md");
 
 	if (!doc || !doc.metadata) {
 		error(404);
