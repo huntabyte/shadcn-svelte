7d3c90a9c3305113731ea75e0e678cb86b5f489d
JabSysEmb <abduljabbar.sha97@gmail.com>
2024-03-15 21:30:11 +0300

    'line-clamp-1' removed and 'overflow' added for select.trigger component

--- apps/www/src/lib/registry/default/example/cards/report-issue.svelte
+++ apps/www/src/lib/registry/default/example/cards/report-issue.svelte
@@ -77,7 +77,7 @@
 			<div class="grid gap-2">
 				<Label for="security-level-{id}">Security Level</Label>
 				<Select.Root selected={securityLevels[1]}>
-					<Select.Trigger id="security-level-{id}" class="line-clamp-1 w-full truncate">
+					<Select.Trigger id="security-level-{id}" class="overflow w-full truncate">
 						<Select.Value placeholder="Select level" />
 					</Select.Trigger>
 					<Select.Content>
