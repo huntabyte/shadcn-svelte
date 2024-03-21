83af86e9a39a823a7dfd20e62d2f4f9d7f61a57e
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-03-21 19:43:00 -0400

    fix pm detecting logic

--- packages/cli/src/utils/get-package-manager.ts
+++ packages/cli/src/utils/get-package-manager.ts
@@ -4,18 +4,19 @@ export async function getPackageManager(
 	targetDir: string
 ): Promise<"yarn" | "pnpm" | "bun" | "npm"> {
 	const packageManager = await detect({ programmatic: true, cwd: targetDir });
+
 	if (packageManager === "yarn@berry") return "yarn";
 	if (packageManager === "pnpm@6") return "pnpm";
 
-	// ni couldn't find a lockfile, so we'll try to determine the PM via the user agent
-	if (packageManager === null) {
-		const userAgent = process.env.npm_config_user_agent;
+	// ni successfully detected a PM
+	if (packageManager !== null) return packageManager;
 
-		if (userAgent === undefined) return "npm";
-		if (userAgent.startsWith("yarn")) return "yarn";
-		if (userAgent.startsWith("pnpm")) return "pnpm";
-	}
+	// ni couldn't find a lockfile, so we'll try to determine the PM via the user agent
+	const userAgent = process.env.npm_config_user_agent;
+	if (userAgent === undefined) return "npm";
+	if (userAgent.startsWith("yarn")) return "yarn";
+	if (userAgent.startsWith("pnpm")) return "pnpm";
 
 	// default to npm as the last resort
-	return packageManager ?? "npm";
+	return "npm";
 }
