8d657363444eb7c67a9ea2a2ac571b3f3e265145
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-03-28 14:47:04 -0400

    replaced tsconfck

--- packages/cli/src/utils/resolve-imports.ts
+++ packages/cli/src/utils/resolve-imports.ts
@@ -1,19 +1,10 @@
-import path from "node:path";
+import { createPathsMatcher, type TsConfigResult } from "get-tsconfig";
 
-type PathAliases = { paths: Record<string, string[]>; pathsBasePath: string };
-export function resolveImport(importPath: string, config: PathAliases) {
-	for (let [alias, paths] of Object.entries(config.paths)) {
-		alias = stripGlob(alias);
-		const aliasPath = paths[0] && stripGlob(paths[0]);
-		if (!importPath.startsWith(alias) || aliasPath === undefined) continue;
-
-		const relativePath = importPath.replace(alias, aliasPath);
-		const resolvedPath = path.resolve(config.pathsBasePath, ...relativePath.split("/"));
-		return resolvedPath;
+export function resolveImport(importPath: string, config: TsConfigResult) {
+	const matcher = createPathsMatcher(config);
+	if (matcher === null) {
+		return;
 	}
-}
-
-function stripGlob(alias: string): string {
-	if (alias.endsWith("/*")) return alias.slice(0, -2);
-	return alias;
+	const paths = matcher(importPath);
+	return paths[0];
 }
