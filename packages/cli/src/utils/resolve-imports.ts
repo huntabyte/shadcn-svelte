8c8874162b650a27b362a839724232fe7c4a1dc2
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-03-28 12:44:47 -0400

    tidy up

--- packages/cli/src/utils/resolve-imports.ts
+++ packages/cli/src/utils/resolve-imports.ts
@@ -1,16 +1,14 @@
 import path from "node:path";
 
-export function resolveImport(
-	importPath: string,
-	config: { paths: Record<string, string[]>; absoluteBaseUrl: string }
-) {
+type PathAliases = { paths: Record<string, string[]>; pathsBasePath: string };
+export function resolveImport(importPath: string, config: PathAliases) {
 	for (let [alias, paths] of Object.entries(config.paths)) {
 		alias = stripGlob(alias);
-		const aliasPath: string | undefined = paths[0] && stripGlob(paths[0]);
+		const aliasPath = paths[0] && stripGlob(paths[0]);
 		if (!importPath.startsWith(alias) || aliasPath === undefined) continue;
 
 		const relativePath = importPath.replace(alias, aliasPath);
-		const resolvedPath = path.resolve(config.absoluteBaseUrl, ...relativePath.split("/"));
+		const resolvedPath = path.resolve(config.pathsBasePath, ...relativePath.split("/"));
 		return resolvedPath;
 	}
 }
