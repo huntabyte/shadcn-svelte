8391ddaf474a31a8f0abaa4ce345e1d0ada808d2
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-03-28 17:09:38 -0400

    unused

--- packages/cli/src/utils/transformers.ts
+++ packages/cli/src/utils/transformers.ts
@@ -1,12 +1,5 @@
 import type { Config } from "./get-config.js";
 
-export type TransformOpts = {
-	filename: string;
-	content: string;
-	config: Config;
-	// baseColor?: z.infer<typeof registryBaseColorSchema>; - will use later
-};
-
 export function transformImports(content: string, config: Config) {
 	let str = content.replace(/\$lib\/registry\/[^/]+/g, config.aliases.components);
 	str = str.replace(/\$lib\/utils/g, config.aliases.utils);
