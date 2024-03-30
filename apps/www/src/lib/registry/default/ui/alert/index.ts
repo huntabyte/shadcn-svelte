dc7418a8e9f8d16b2d4b62e29ca73dec89ef6205
Hunter Johnston <johnstonhuntera@gmail.com>
2024-03-30 00:01:10 -0400

    formatting

--- apps/www/src/lib/registry/default/ui/alert/index.ts
+++ apps/www/src/lib/registry/default/ui/alert/index.ts
@@ -5,13 +5,13 @@ import Description from "./alert-description.svelte";
 import Title from "./alert-title.svelte";
 
 export const alertVariants = tv({
-	base: "relative w-full rounded-lg border p-4 [&>svg]:absolute [&>svg]:text-foreground [&>svg]:left-4 [&>svg]:top-4 [&>svg+div]:translate-y-[-3px] [&:has(svg)]:pl-11",
+	base: "relative w-full rounded-lg border p-4 [&:has(svg)]:pl-11 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
 
 	variants: {
 		variant: {
 			default: "bg-background text-foreground",
 			destructive:
-				"text-destructive border-destructive/50 dark:border-destructive [&>svg]:text-destructive text-destructive",
+				"border-destructive/50 text-destructive text-destructive dark:border-destructive [&>svg]:text-destructive",
 		},
 	},
 	defaultVariants: {
--- apps/www/src/lib/registry/new-york/ui/badge/index.ts
+++ apps/www/src/lib/registry/new-york/ui/badge/index.ts
@@ -2,7 +2,7 @@ import { tv, type VariantProps } from "tailwind-variants";
 
 export { default as Badge } from "./badge.svelte";
 export const badgeVariants = tv({
-	base: "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 select-none",
+	base: "inline-flex select-none items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
 	variants: {
 		variant: {
 			default:
