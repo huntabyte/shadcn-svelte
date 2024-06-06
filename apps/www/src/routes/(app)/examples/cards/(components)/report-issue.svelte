b7bd9b00912b3864f6dcbb110f903d8c5a95e57e
hansaskov <hansaskov1999@gmail.com>
2024-06-06 11:32:27 +0200

    Run pnpm format

--- apps/www/src/routes/(app)/examples/cards/(components)/report-issue.svelte
+++ apps/www/src/routes/(app)/examples/cards/(components)/report-issue.svelte
@@ -74,7 +74,10 @@
 			<div class="grid gap-2">
 				<Label for="security-level">Security Level</Label>
 				<Select.Root selected={securityLevels[1]}>
-					<Select.Trigger id="security-level" class="line-clamp-1 flex w-[160px] truncate">
+					<Select.Trigger
+						id="security-level"
+						class="line-clamp-1 flex w-[160px] truncate"
+					>
 						<Select.Value placeholder="Select level" />
 					</Select.Trigger>
 					<Select.Content>
