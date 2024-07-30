081b83863262695f32ba0578ff2b4405a8d487ab
Magnar Ovedal Myrtveit <magnar@myrtveit.com>
2024-07-30 04:57:21 +0200

    fix: Vertical separator height (#1206)

--- sites/docs/src/lib/registry/default/ui/separator/separator.svelte
+++ sites/docs/src/lib/registry/default/ui/separator/separator.svelte
@@ -13,7 +13,7 @@
 <SeparatorPrimitive.Root
 	class={cn(
 		"bg-border shrink-0",
-		orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+		orientation === "horizontal" ? "h-[1px] w-full" : "min-h-full w-[1px]",
 		className
 	)}
 	{orientation}
