8c26c517565043e65c894fdbbcbee24ce439202c
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-09-02 16:23:15 +0200

    lint/format

--- sites/docs/src/routes/(app)/examples/tasks-2/(data)/data.ts
+++ sites/docs/src/routes/(app)/examples/tasks-2/(data)/data.ts
@@ -1,69 +1,69 @@
-import ArrowDown from 'svelte-radix/ArrowDown.svelte';
-import ArrowRight from 'svelte-radix/ArrowRight.svelte';
-import ArrowUp from 'svelte-radix/ArrowUp.svelte';
-import CheckCircled from 'svelte-radix/CheckCircled.svelte';
-import Circle from 'svelte-radix/Circle.svelte';
-import CrossCircled from 'svelte-radix/CrossCircled.svelte';
-import QuestionMarkCircled from 'svelte-radix/QuestionMarkCircled.svelte';
-import Stopwatch from 'svelte-radix/Stopwatch.svelte';
+import ArrowDown from "svelte-radix/ArrowDown.svelte";
+import ArrowRight from "svelte-radix/ArrowRight.svelte";
+import ArrowUp from "svelte-radix/ArrowUp.svelte";
+import CheckCircled from "svelte-radix/CheckCircled.svelte";
+import Circle from "svelte-radix/Circle.svelte";
+import CrossCircled from "svelte-radix/CrossCircled.svelte";
+import QuestionMarkCircled from "svelte-radix/QuestionMarkCircled.svelte";
+import Stopwatch from "svelte-radix/Stopwatch.svelte";
 
 export const labels = [
 	{
-		value: 'bug',
-		label: 'Bug'
+		value: "bug",
+		label: "Bug",
 	},
 	{
-		value: 'feature',
-		label: 'Feature'
+		value: "feature",
+		label: "Feature",
 	},
 	{
-		value: 'documentation',
-		label: 'Documentation'
-	}
+		value: "documentation",
+		label: "Documentation",
+	},
 ];
 
 export const statuses = [
 	{
-		value: 'backlog',
-		label: 'Backlog',
-		icon: QuestionMarkCircled
+		value: "backlog",
+		label: "Backlog",
+		icon: QuestionMarkCircled,
 	},
 	{
-		value: 'todo',
-		label: 'Todo',
-		icon: Circle
+		value: "todo",
+		label: "Todo",
+		icon: Circle,
 	},
 	{
-		value: 'in progress',
-		label: 'In Progress',
-		icon: Stopwatch
+		value: "in progress",
+		label: "In Progress",
+		icon: Stopwatch,
 	},
 	{
-		value: 'done',
-		label: 'Done',
-		icon: CheckCircled
+		value: "done",
+		label: "Done",
+		icon: CheckCircled,
 	},
 	{
-		value: 'canceled',
-		label: 'Canceled',
-		icon: CrossCircled
-	}
+		value: "canceled",
+		label: "Canceled",
+		icon: CrossCircled,
+	},
 ];
 
 export const priorities = [
 	{
-		label: 'Low',
-		value: 'low',
-		icon: ArrowDown
+		label: "Low",
+		value: "low",
+		icon: ArrowDown,
 	},
 	{
-		label: 'Medium',
-		value: 'medium',
-		icon: ArrowRight
+		label: "Medium",
+		value: "medium",
+		icon: ArrowRight,
 	},
 	{
-		label: 'High',
-		value: 'high',
-		icon: ArrowUp
-	}
+		label: "High",
+		value: "high",
+		icon: ArrowUp,
+	},
 ];
--- sites/docs/src/routes/(app)/examples/tasks-2/(data)/schema.ts
+++ sites/docs/src/routes/(app)/examples/tasks-2/(data)/schema.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 // We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
@@ -7,7 +7,7 @@ export const taskSchema = z.object({
 	title: z.string(),
 	status: z.string(),
 	label: z.string(),
-	priority: z.string()
+	priority: z.string(),
 });
 
 export type Task = z.infer<typeof taskSchema>;
