f91c73270cbe5a4c172f08e0ec9ca634d4ae2e35
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-09-25 14:56:41 +0200

    make sure to pick up/apply layerchart styles (thanks @techniq)

--- sites/docs/tailwind.config.js
+++ sites/docs/tailwind.config.js
@@ -3,7 +3,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 const config = {
 	darkMode: ["class"],
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/layerchart/**/*.{svelte,js}"],
 	safelist: ["dark"],
 	theme: {
 		container: {
