a0453bc60316a525e5c741370faaee762e467c96
Hunter Johnston <johnstonhuntera@gmail.com>
2024-10-03 14:32:33 -0400

    chore: carbon

---
+++ sites/docs/src/lib/components/docs/carbon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+{#if $page.url.pathname}
+	<script
+		async
+		type="text/javascript"
+		src="//cdn.carbonads.com/carbon.js?serve=CW7DK27L&placement=shadcn-sveltecom&format=classic"
+		id="_carbonads_js"
+	></script>
+{/if}
