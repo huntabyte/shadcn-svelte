b3359f97154d7940148b3c3d647d6ff501f0e7e4
Hunter Johnston <johnstonhuntera@gmail.com>
2024-10-13 23:48:33 -0400

    update docs for tanstack

--- sites/docs/src/lib/registry/default/example/data-table/data-table-actions.svelte
+++ sites/docs/src/lib/registry/default/example/data-table/data-table-actions.svelte
@@ -1,4 +1,4 @@
-<script lang="ts" generics="TData">
+<script lang="ts">
 	import Ellipsis from "lucide-svelte/icons/ellipsis";
 	import { Button } from "$lib/registry/default/ui/button/index.js";
 	import * as DropdownMenu from "$lib/registry/default/ui/dropdown-menu/index.js";
