5eb302262ae72763ef946f89d6fea4b2bf55b6a1
Hunter Johnston <johnstonhuntera@gmail.com>
2024-10-19 22:19:48 -0400

    next stuff

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -251,7 +251,7 @@ async function runAdd(cwd: string, config: Config, options: AddOptions) {
 			title: `${highlight(pm)}: Installing dependencies`,
 			enabled: dependencies.size > 0,
 			async task() {
-				await execa(pm, [add, ...dependencies], {
+				await execa(pm, [add, "-D", ...dependencies], {
 					cwd,
 				});
 				return `Dependencies installed with ${highlight(pm)}`;
