a4ce4fa87e2b535c1fdb1584a3c3027a4d1deb77
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2024-10-25 17:25:08 -0400

    fix: select scroll up button (#1350)

--- sites/docs/src/lib/registry/new-york/ui/select/select-scroll-up-button.svelte
+++ sites/docs/src/lib/registry/new-york/ui/select/select-scroll-up-button.svelte
@@ -10,10 +10,10 @@
 	}: WithoutChildrenOrChild<SelectPrimitive.ScrollDownButtonProps> = $props();
 </script>
 
-<SelectPrimitive.ScrollDownButton
+<SelectPrimitive.ScrollUpButton
 	bind:ref
 	class={cn("flex cursor-default items-center justify-center py-1", className)}
 	{...restProps}
 >
 	<ChevronUp class="size-4" />
-</SelectPrimitive.ScrollDownButton>
+</SelectPrimitive.ScrollUpButton>
