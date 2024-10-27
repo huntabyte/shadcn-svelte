7c566798cc4d2a2f976164930e3bd08480e91202
Hunter Johnston <johnstonhuntera@gmail.com>
2024-10-27 12:06:45 -0400

    fix: use Svelte types

--- sites/docs/src/lib/registry/default/ui/drawer/drawer-footer.svelte
+++ sites/docs/src/lib/registry/default/ui/drawer/drawer-footer.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import { type PrimitiveDivAttributes, cn } from "$lib/utils.js";
+	import { cn } from "$lib/utils.js";
+	import type { WithElementRef } from "bits-ui";
+	import type { HTMLAttributes } from "svelte/elements";
 
 	let {
 		ref = $bindable(null),
 		class: className,
 		children,
 		...restProps
-	}: PrimitiveDivAttributes = $props();
+	}: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
 </script>
 
 <div bind:this={ref} class={cn("mt-auto flex flex-col gap-2 p-4", className)} {...restProps}>
