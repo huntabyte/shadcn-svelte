7eefedc7214dcd16a85b9c11a9ca6aaa634ffce5
Hunter Johnston <johnstonhuntera@gmail.com>
2024-10-27 21:32:53 -0400

    fix: mobile blocks

--- sites/docs/src/lib/components/docs/component-preview-manual.svelte
+++ sites/docs/src/lib/components/docs/component-preview-manual.svelte
@@ -22,20 +22,20 @@
 
 {#if type === "block"}
 	<div class="relative aspect-[4/2.5] w-full overflow-hidden rounded-md border">
-		<!-- <Image
-	  src={`/images/blocks/${name}.png`}
-	  alt={name}
-	  width={1440}
-	  height={900}
-	  class="absolute left-0 top-0 z-20 w-[970px] max-w-none bg-background dark:hidden sm:w-[1280px] md:hidden md:dark:hidden"
-	/>
-	<Image
-	  src={`/images/blocks/${name}-dark.png`}
-	  alt={name}
-	  width={1440}
-	  height={900}
-	  class="absolute left-0 top-0 z-20 hidden w-[970px] max-w-none bg-background dark:block sm:w-[1280px] md:hidden md:dark:hidden"
-	/> -->
+		<img
+			src={`/images/blocks/${name}.png`}
+			alt={name}
+			width={1440}
+			height={900}
+			class="bg-background absolute left-0 top-0 z-20 w-[970px] max-w-none sm:w-[1280px] md:hidden dark:hidden md:dark:hidden"
+		/>
+		<img
+			src={`/images/blocks/${name}-dark.png`}
+			alt={name}
+			width={1440}
+			height={900}
+			class="bg-background absolute left-0 top-0 z-20 hidden w-[970px] max-w-none sm:w-[1280px] md:hidden dark:block md:dark:hidden"
+		/>
 		<div class="bg-background absolute inset-0 hidden w-[1600px] md:block">
 			<iframe {title} src={`/blocks/${$config.style}/${name}`} class="size-full"></iframe>
 		</div>
