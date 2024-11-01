658842fd840c9d0e691a163d3bfccbc81e3a54e2
Aidan Bleser <aidanbleser35@gmail.com>
2024-11-01 09:45:23 -0500

    Add utils update to task list

--- packages/cli/src/commands/update.ts
+++ packages/cli/src/commands/update.ts
@@ -149,10 +149,18 @@ async function runUpdate(cwd: string, config: cliConfig.Config, options: UpdateO
 		if (p.isCancel(proceed) || proceed === false) cancel();
 	}
 
+	const tasks: p.Task[] = [];
+
 	const utilsIndex = selectedComponents.findIndex((item) => item.name === "utils");
 
 	// `update utils` - update the utils.(ts|js) file
 	if (utilsIndex !== -1) {
+		// remove utils so that it isn't fetched from the registry
+		selectedComponents = [
+			...selectedComponents.slice(0, utilsIndex),
+			...selectedComponents.slice(utilsIndex + 1),
+		];
+
 		const extension = config.typescript ? ".ts" : ".js";
 		const utilsPath = config.resolvedPaths.utils + extension;
 
@@ -160,14 +168,13 @@ async function runUpdate(cwd: string, config: cliConfig.Config, options: UpdateO
 			throw error(`Failed to find ${highlight("utils")} at ${color.cyan(utilsPath)}`);
 		}
 
-		// utils.(ts|js) is not in the registry, it is a template, so we'll just overwrite it
-		await fs.writeFile(utilsPath, config.typescript ? UTILS : UTILS_JS);
-
-		// remove utils so that it isn't fetched from the registry
-		selectedComponents = [
-			...selectedComponents.slice(0, utilsIndex),
-			...selectedComponents.slice(utilsIndex + 1),
-		];
+		tasks.push({
+			title: `Updating ${highlight(utilsPath)}`,
+			async task() {
+				// utils.(ts|js) is not in the registry, it is a template, so we'll just overwrite it
+				await fs.writeFile(utilsPath, config.typescript ? UTILS : UTILS_JS);
+			},
+		});
 	}
 
 	const tree = await registry.resolveTree({
@@ -181,7 +188,6 @@ async function runUpdate(cwd: string, config: cliConfig.Config, options: UpdateO
 
 	const componentsToRemove: Record<string, string[]> = {};
 	const dependencies = new Set<string>();
-	const tasks: p.Task[] = [];
 	for (const item of payload) {
 		const targetDir = registry.getItemTargetPath(config, item);
 		if (!targetDir) {
