181e7dbdd64ba9ce946c22a51039e1105de7f354
Cory Tyburski <corytyburski@gmail.com>
2024-11-04 20:07:13 -0500

    Fix stacked margin classes
    CSS classes were not being properly recognized by UnoCSS.

--- sites/docs/src/lib/registry/new-york/ui/sidebar/sidebar-inset.svelte
+++ sites/docs/src/lib/registry/new-york/ui/sidebar/sidebar-inset.svelte
@@ -15,7 +15,7 @@
 	bind:this={ref}
 	class={cn(
 		"bg-background relative flex min-h-svh flex-1 flex-col",
-		"peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
+		"peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-[[data-state=collapsed][data-variant=inset]]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
 		className
 	)}
 	{...restProps}
