9e47cde3a7c65ced02f0c9b84cb0530b78c313a4
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-11-15 11:36:42 -0500

    fix self-reference

--- sites/docs/src/lib/registry/default/ui/data-table/flex-render.svelte
+++ sites/docs/src/lib/registry/default/ui/data-table/flex-render.svelte
@@ -10,10 +10,7 @@
 	lang="ts"
 	generics="TData, TValue, TContext extends HeaderContext<TData, TValue> | CellContext<TData, TValue>"
 >
-	import {
-		RenderComponentConfig,
-		RenderSnippetConfig,
-	} from "$lib/registry/default/ui/data-table/render-helpers.js";
+	import { RenderComponentConfig, RenderSnippetConfig } from "./render-helpers.js";
 	type Props = {
 		/** The cell or header field of the current cell's column definition. */
 		content?: TContext extends HeaderContext<TData, TValue>
