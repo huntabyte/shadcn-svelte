814429e673d753dcbd4c475349c65d952d02aa1c
Cory Tyburski <corytyburski@gmail.com>
2024-11-15 12:43:27 -0500

    Add portal props to context menu & dropdown menu

--- sites/docs/src/lib/registry/default/ui/context-menu/context-menu-content.svelte
+++ sites/docs/src/lib/registry/default/ui/context-menu/context-menu-content.svelte
@@ -4,16 +4,21 @@
 
 	let {
 		ref = $bindable(null),
+		portalProps,
 		class: className,
 		...restProps
-	}: ContextMenuPrimitive.ContentProps = $props();
+	}: ContextMenuPrimitive.ContentProps & {
+		portalProps?: ContextMenuPrimitive.PortalProps;
+	} = $props();
 </script>
 
-<ContextMenuPrimitive.Content
-	bind:ref
-	class={cn(
-		"bg-popover text-popover-foreground z-50 min-w-[8rem] rounded-md border p-1 shadow-md focus:outline-none",
-		className
-	)}
-	{...restProps}
-/>
+<ContextMenuPrimitive.Portal {...portalProps}>
+	<ContextMenuPrimitive.Content
+		bind:ref
+		class={cn(
+			"bg-popover text-popover-foreground z-50 min-w-[8rem] rounded-md border p-1 shadow-md focus:outline-none",
+			className
+		)}
+		{...restProps}
+	/>
+</ContextMenuPrimitive.Portal>
