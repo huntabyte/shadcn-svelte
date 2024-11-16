9192a5bccda2ed2b692f1663891f0cd3324b2b7a
Cory Tyburski <corytyburski@gmail.com>
2024-11-16 16:23:02 -0500

    Add portal to menubar

--- sites/docs/src/lib/registry/default/ui/menubar/menubar-content.svelte
+++ sites/docs/src/lib/registry/default/ui/menubar/menubar-content.svelte
@@ -9,19 +9,24 @@
 		alignOffset = -4,
 		align = "start",
 		side = "bottom",
+		portalProps,
 		...restProps
-	}: MenubarPrimitive.ContentProps = $props();
+	}: MenubarPrimitive.ContentProps & {
+		portalProps?: MenubarPrimitive.PortalProps;
+	} = $props();
 </script>
 
-<MenubarPrimitive.Content
-	bind:ref
-	{sideOffset}
-	{align}
-	{alignOffset}
-	{side}
-	class={cn(
-		"bg-popover text-popover-foreground z-50 min-w-[12rem] rounded-md border p-1 shadow-md focus:outline-none",
-		className
-	)}
-	{...restProps}
-/>
+<MenubarPrimitive.Portal {...portalProps}>
+	<MenubarPrimitive.Content
+		bind:ref
+		{sideOffset}
+		{align}
+		{alignOffset}
+		{side}
+		class={cn(
+			"bg-popover text-popover-foreground z-50 min-w-[12rem] rounded-md border p-1 shadow-md focus:outline-none",
+			className
+		)}
+		{...restProps}
+	/>
+</MenubarPrimitive.Portal>
--- sites/docs/src/lib/registry/new-york/ui/menubar/menubar-content.svelte
+++ sites/docs/src/lib/registry/new-york/ui/menubar/menubar-content.svelte
@@ -8,18 +8,23 @@
 		sideOffset = 8,
 		alignOffset = -4,
 		align = "start",
+		portalProps,
 		...restProps
-	}: MenubarPrimitive.ContentProps = $props();
+	}: MenubarPrimitive.ContentProps & {
+		portalProps?: MenubarPrimitive.PortalProps;
+	} = $props();
 </script>
 
-<MenubarPrimitive.Content
-	bind:ref
-	{sideOffset}
-	{align}
-	{alignOffset}
-	class={cn(
-		"bg-popover text-popover-foreground z-50 min-w-[12rem] rounded-md border p-1 shadow-md focus:outline-none",
-		className
-	)}
-	{...restProps}
-/>
+<MenubarPrimitive.Portal {...portalProps}>
+	<MenubarPrimitive.Content
+		bind:ref
+		{sideOffset}
+		{align}
+		{alignOffset}
+		class={cn(
+			"bg-popover text-popover-foreground z-50 min-w-[12rem] rounded-md border p-1 shadow-md focus:outline-none",
+			className
+		)}
+		{...restProps}
+	/>
+</MenubarPrimitive.Portal>
