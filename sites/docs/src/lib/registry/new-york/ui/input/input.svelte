e9011e23a26e492532080bb8d8e8a56cfc81267d
Aidan Bleser <aidanbleser35@gmail.com>
2024-12-12 06:13:10 -0600

    update sizes and bump bits

--- sites/docs/src/lib/registry/new-york/ui/input/input.svelte
+++ sites/docs/src/lib/registry/new-york/ui/input/input.svelte
@@ -14,7 +14,7 @@
 <input
 	bind:this={ref}
 	class={cn(
-		"border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+		"border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base sm:text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-base sm:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
 		className
 	)}
 	bind:value
