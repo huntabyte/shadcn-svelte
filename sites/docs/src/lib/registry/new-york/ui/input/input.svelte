2fd0af23ff07650f46154c5fe70bbaedb364de2a
Aidan Bleser <aidanbleser35@gmail.com>
2024-12-12 06:22:27 -0600

    Update input.svelte

--- sites/docs/src/lib/registry/new-york/ui/input/input.svelte
+++ sites/docs/src/lib/registry/new-york/ui/input/input.svelte
@@ -14,10 +14,9 @@
 <input
 	bind:this={ref}
 	class={cn(
-		"border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-base file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
+		"border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
 		className
 	)}
-	bind:val}
 	bind:value
-	{.../>}
+	{...restProps}
 />
