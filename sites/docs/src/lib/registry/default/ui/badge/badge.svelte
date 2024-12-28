6d607290d75bf1037476a0e3e8b8a42836cc140a
Aidan Bleser <aidanbleser35@gmail.com>
2024-12-28 07:26:56 -0600

    bump svelte + fix className positioning

--- sites/docs/src/lib/registry/default/ui/badge/badge.svelte
+++ sites/docs/src/lib/registry/default/ui/badge/badge.svelte
@@ -43,7 +43,7 @@
 	this={href ? "a" : "span"}
 	bind:this={ref}
 	{href}
-	class={cn(badgeVariants({ variant, className }))}
+	class={cn(badgeVariants({ variant }), className)}
 	{...restProps}
 >
 	{@render children?.()}
