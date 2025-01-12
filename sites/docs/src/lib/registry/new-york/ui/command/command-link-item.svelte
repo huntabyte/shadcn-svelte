19c9213c90f3e15ff450ad9f01aff60a258c0859
witer33 <34513257+witer33@users.noreply.github.com>
2025-01-12 19:27:19 +0000

    fix: added svg css to command-link-item

--- sites/docs/src/lib/registry/new-york/ui/command/command-link-item.svelte
+++ sites/docs/src/lib/registry/new-york/ui/command/command-link-item.svelte
@@ -11,7 +11,7 @@
 
 <CommandPrimitive.LinkItem
 	class={cn(
-		"aria-selected:bg-accent aria-selected:text-accent-foreground relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+		"aria-selected:bg-accent aria-selected:text-accent-foreground relative flex cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
 		className
 	)}
 	bind:ref
