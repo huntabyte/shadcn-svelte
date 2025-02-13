e4827282cdc5aa96015a6441c12bf01581e86735
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2025-02-12 20:33:48 -0500

    next: bits 1.0 (#1685)

--- sites/docs/scripts/tmp.ts
+++ sites/docs/scripts/tmp.ts
@@ -1,2 +1,2 @@
 // will be removed once moving from `next` to `latest`
-export const TMP_NEXT_DEPS = ["bits-ui", "paneforge", "vaul-svelte"];
+export const TMP_NEXT_DEPS = ["paneforge", "vaul-svelte"];
