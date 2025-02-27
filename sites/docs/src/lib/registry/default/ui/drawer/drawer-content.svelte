626777bcc6ea478c27dfab57fa7eb7068cecf054
Artem Gurtovoi <tema.gurtovoy@gmail.com>
2025-02-27 13:12:56 +0400

    fix(drawer): add bottom safe inset
    When app is installed (as pwa) in standalone mode, the most-bottom button overlaps with "home bar" on iPhone.

--- sites/docs/src/lib/registry/default/ui/drawer/drawer-content.svelte
+++ sites/docs/src/lib/registry/default/ui/drawer/drawer-content.svelte
@@ -13,7 +13,7 @@
 	<DrawerOverlay />
 	<DrawerPrimitive.Content
 		class={cn(
-			"bg-background fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border",
+			"bg-background fixed inset-x-0 bottom-[env(safe-area-inset-bottom)] z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border",
 			className
 		)}
 		{...$$restProps}
