95dca5883820b8bb7283da9db154d1eac634b05e
Aidan Bleser <aidanbleser35@gmail.com>
2025-03-18 15:39:24 -0500

    format

--- packages/cli/test/utils/registry/index.spec.ts
+++ packages/cli/test/utils/registry/index.spec.ts
@@ -1,9 +1,6 @@
 import path from "node:path";
 import { describe, expect, it } from "vitest";
-import {
-	getItemTargetPath,
-	getRegistryItemPath,
-} from "../../../src/utils/registry/index";
+import { getItemTargetPath, getRegistryItemPath } from "../../../src/utils/registry/index";
 import { SITE_BASE_URL } from "../../../src/constants";
 
 const config = {
