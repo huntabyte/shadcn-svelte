e1a4403a97e4084ca87f90ced75214835bc4f069
Aidan Bleser <117548273+ieedan@users.noreply.github.com>
2025-03-20 08:58:24 -0500

    export portal prop (#1780)

--- sites/docs/src/lib/registry/ui/tooltip/index.ts
+++ sites/docs/src/lib/registry/ui/tooltip/index.ts
@@ -4,15 +4,18 @@ import Content from "./tooltip-content.svelte";
 const Root = TooltipPrimitive.Root;
 const Trigger = TooltipPrimitive.Trigger;
 const Provider = TooltipPrimitive.Provider;
+const Portal = TooltipPrimitive.Portal;
 
 export {
 	Root,
 	Trigger,
 	Content,
 	Provider,
+	Portal,
 	//
 	Root as Tooltip,
 	Content as TooltipContent,
 	Trigger as TooltipTrigger,
 	Provider as TooltipProvider,
+	Portal as TooltipPortal,
 };
