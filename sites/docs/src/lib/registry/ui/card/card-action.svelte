7ac8b95492a8dd6a5723bf2c9d818ca559ddf7ad
Hunter Johnston <johnstonhuntera@gmail.com>
2025-03-20 20:49:48 -0400

    ty eslint

--- sites/docs/src/lib/registry/ui/card/card-action.svelte
+++ sites/docs/src/lib/registry/ui/card/card-action.svelte
@@ -15,6 +15,7 @@
 	bind:this={ref}
 	data-slot="card-action"
 	class={cn("col-start-2 row-span-2 row-start-1 self-start justify-self-end", className)}
+	{...restProps}
 >
 	{@render children?.()}
 </div>
