b0feaeee2e674a146659ff6527d7c7af8835b764
Aidan Bleser <aidanbleser35@gmail.com>
2025-03-24 15:30:30 -0500

    format

--- sites/docs/src/lib/registry/ui/drawer/drawer-trigger.svelte
+++ sites/docs/src/lib/registry/ui/drawer/drawer-trigger.svelte
@@ -2,7 +2,8 @@
 	import type { ComponentProps } from "svelte";
 	import { Drawer as DrawerPrimitive } from "vaul-svelte";
 
-	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof DrawerPrimitive.Trigger> = $props();
+	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof DrawerPrimitive.Trigger> =
+		$props();
 </script>
 
 <DrawerPrimitive.Trigger bind:ref data-slot="drawer-trigger" {...restProps} />
