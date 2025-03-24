058e06d9b8a2fd2637fee50cfc2f2485b891f117
Aidan Bleser <aidanbleser35@gmail.com>
2025-03-24 13:53:12 -0500

    comp: Drawer

---
+++ sites/docs/src/lib/registry/ui/drawer/drawer-close.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Drawer as DrawerPrimitive } from "vaul-svelte";
+
+	let restProps: DrawerPrimitive.CloseProps = $props();
+</script>
+
+<DrawerPrimitive.Close data-slot="drawer-close" {...restProps} />
---
+++ sites/docs/src/lib/registry/ui/drawer/drawer-trigger.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Drawer as DrawerPrimitive } from "vaul-svelte";
+
+	let restProps: DrawerPrimitive.TriggerProps = $props();
+</script>
+
+<DrawerPrimitive.Trigger data-slot="drawer-trigger" {...restProps} />
