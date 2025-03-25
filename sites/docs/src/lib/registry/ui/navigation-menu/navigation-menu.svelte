ca5e0193da20bfec0efd05d7c4be5bdc33e99f97
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-03-25 21:08:24 +0200

    viewport

--- sites/docs/src/lib/registry/ui/navigation-menu/navigation-menu.svelte
+++ sites/docs/src/lib/registry/ui/navigation-menu/navigation-menu.svelte
@@ -5,14 +5,18 @@
 	let {
 		ref = $bindable(null),
 		class: className,
+		viewport = true,
 		children,
 		...restProps
-	}: NavigationMenuPrimitive.RootProps = $props();
+	}: NavigationMenuPrimitive.RootProps & {
+		viewport?: boolean;
+	} = $props();
 </script>
 
 <NavigationMenuPrimitive.Root
 	bind:ref
 	data-slot="navigation-menu"
+	data-viewport={viewport}
 	class={cn(
 		"group/navigation-menu relative flex max-w-max flex-1 items-center justify-center",
 		className
@@ -20,15 +24,18 @@
 	{...restProps}
 >
 	{@render children?.()}
-	<div class={cn("absolute left-0 top-full isolate z-50 flex justify-center")}>
-		<NavigationMenuPrimitive.Viewport
-			bind:ref
-			data-slot="navigation-menu-viewport"
-			class={cn(
-				"origin-top-center bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 relative mt-1.5 h-[var(--bits-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border shadow md:w-[var(--bits-navigation-menu-viewport-width)]",
-				className
-			)}
-			{...restProps}
-		/>
-	</div>
+
+	{#if viewport}
+		<div class={cn("absolute left-0 top-full isolate z-50 flex justify-center")}>
+			<NavigationMenuPrimitive.Viewport
+				bind:ref
+				data-slot="navigation-menu-viewport"
+				class={cn(
+					"origin-top-center bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 relative mt-1.5 h-[var(--bits-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border shadow md:w-[var(--bits-navigation-menu-viewport-width)]",
+					className
+				)}
+				{...restProps}
+			/>
+		</div>
+	{/if}
 </NavigationMenuPrimitive.Root>
