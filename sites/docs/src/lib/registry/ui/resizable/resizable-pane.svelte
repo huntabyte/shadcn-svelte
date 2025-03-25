7a0b62c0e4443169c8d4e01c114ace9a8a25bb1d
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-03-25 17:20:41 +0200

    no need for data-slot for root pane

--- sites/docs/src/lib/registry/ui/resizable/resizable-pane.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import * as ResizablePanePrimitive from "paneforge";
-
-	let { ref = $bindable(null), ...restProps }: ResizablePanePrimitive.PaneProps = $props();
-</script>
-
-<ResizablePanePrimitive.Pane bind:ref data-slot="resizable-pane" {...restProps} />
