2f5b706b37449685a3defad67b2b9b20c8ef2d05
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-03-25 12:16:22 +0200

    Resizable

---
+++ sites/docs/src/lib/registry/ui/resizable/resizable-pane.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import * as ResizablePrimitive from "paneforge";
+
+	let { ...restProps }: ResizablePrimitive.PaneProps = $props();
+</script>
+
+<ResizablePrimitive.Pane data-slot="resizable-pane" {...restProps} />
