fdb0186ba43f2966825a80fb19d2ddc20abc846f
Aidan Bleser <aidanbleser35@gmail.com>
2025-03-25 05:27:23 -0500

    Update drawer-trigger.svelte

--- sites/docs/src/lib/registry/ui/drawer/drawer-trigger.svelte
+++ sites/docs/src/lib/registry/ui/drawer/drawer-trigger.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import type { ComponentProps } from "svelte";
 	import { Drawer as DrawerPrimitive } from "vaul-svelte";
 
-	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof DrawerPrimitive.Trigger> =
-		$props();
+	let { ref = $bindable(null), ...restProps }: DrawerPrimitive.TriggerProps = $props();
 </script>
 
 <DrawerPrimitive.Trigger bind:ref data-slot="drawer-trigger" {...restProps} />
