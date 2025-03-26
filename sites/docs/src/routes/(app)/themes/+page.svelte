01a9bc15b0bfd86f715a8c39dfd9d1bb68684975
Aidan Bleser <aidanbleser35@gmail.com>
2025-03-26 14:43:19 -0500

    idk

--- sites/docs/src/routes/(app)/themes/+page.svelte
+++ sites/docs/src/routes/(app)/themes/+page.svelte
@@ -4,7 +4,6 @@
 	import ThemeWrapper from "$lib/components/docs/theme-wrapper.svelte";
 	import * as PageHeader from "$lib/components/docs/page-header/index.js";
 	import { CardsDefault } from "$lib/registry/example/cards/index.js";
-	import "../../../styles/themes.css";
 </script>
 
 <svelte:head>
