67cc976775132adab55cd0cbd659fa3a86ec69fb
Aidan Bleser <aidanbleser35@gmail.com>
2025-03-26 10:25:16 -0500

    unf*ck the layout

--- sites/docs/src/lib/components/docs/site-header.svelte
+++ sites/docs/src/lib/components/docs/site-header.svelte
@@ -10,9 +10,9 @@
 </script>
 
 <header
-	class="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur"
+	class="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b border-dashed backdrop-blur"
 >
-	<div class="container flex h-14 max-w-screen-2xl items-center">
+	<div class="container !pb-0 flex h-14 max-w-screen-2xl items-center">
 		<MainNav />
 		<MobileNav />
 		<div class="flex flex-1 items-center justify-between space-x-2 md:justify-end">
