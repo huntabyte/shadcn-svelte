78102fde7d771dda19244ea642a15857a0610f9c
Magnus Markling <magnus@markling.com>
2025-03-27 17:19:29 +0000

    fix: add each-key to slider (#1731)
    resolves this lint error
    https://sveltejs.github.io/eslint-plugin-svelte/rules/require-each-key/

--- sites/docs/src/lib/registry/new-york/ui/slider/slider.svelte
+++ sites/docs/src/lib/registry/new-york/ui/slider/slider.svelte
@@ -18,7 +18,7 @@
 	<span class="bg-primary/20 relative h-1.5 w-full grow overflow-hidden rounded-full">
 		<SliderPrimitive.Range class="bg-primary absolute h-full" />
 	</span>
-	{#each thumbs as thumb}
+	{#each thumbs as thumb (thumb)}
 		<SliderPrimitive.Thumb
 			{thumb}
 			class="border-primary/50 bg-background focus-visible:ring-ring block h-4 w-4 rounded-full border shadow transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
