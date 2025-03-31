6c5fc54001d83463a463977aa0cc96f1401cbdaa
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-03-31 22:30:14 +0200

    update tooltip-content

--- sites/docs/src/lib/registry/ui/tooltip/tooltip-content.svelte
+++ sites/docs/src/lib/registry/ui/tooltip/tooltip-content.svelte
@@ -6,6 +6,7 @@
 		ref = $bindable(null),
 		class: className,
 		sideOffset = 0,
+		side,
 		children,
 		...restProps
 	}: TooltipPrimitive.ContentProps = $props();
@@ -16,6 +17,7 @@
 		bind:ref
 		data-slot="tooltip-content"
 		{sideOffset}
+		{side}
 		class={cn(
 			"bg-primary text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--bits-tooltip-content-transform-origin) z-50 w-fit text-balance rounded-md px-3 py-1.5 text-xs",
 			className
@@ -23,8 +25,19 @@
 		{...restProps}
 	>
 		{@render children?.()}
-		<TooltipPrimitive.Arrow
-			class="bg-primary fill-primary z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]"
-		/>
+		<TooltipPrimitive.Arrow>
+			{#snippet child({ props })}
+				<div
+					{...props}
+					class={cn(
+						"bg-primary top z-50 size-2.5 rotate-45 rounded-[2px]",
+						side === "top" && "translate-x-1/2 translate-y-[calc(-50%_+_2px)]",
+						side === "bottom" && "-translate-x-1/2 -translate-y-[calc(-50%_+_1px)]",
+						side === "right" && "translate-x-[calc(50%_+_2px)] translate-y-1/2",
+						side === "left" && "-translate-y-[calc(50%_-_3px)]"
+					)}
+				></div>
+			{/snippet}
+		</TooltipPrimitive.Arrow>
 	</TooltipPrimitive.Content>
 </TooltipPrimitive.Portal>
