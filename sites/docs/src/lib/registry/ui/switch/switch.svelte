0c47ffee949e5a6654fc0fe011d268ffe4ddb100
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-03-31 22:40:59 +0200

    shame on me for try to resolve conflicts in zed (good editor, though)

--- sites/docs/src/lib/registry/ui/switch/switch.svelte
+++ sites/docs/src/lib/registry/ui/switch/switch.svelte
@@ -15,11 +15,7 @@
 	bind:checked
 	data-slot="switch"
 	class={cn(
-<<<<<<< HEAD
 		"data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 shadow-xs peer inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent outline-none transition-all focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
-=======
-		"focus-visible:ring-ring focus-visible:ring-offset-background data-[state=checked]:bg-primary data-[state=unchecked]:bg-input peer inline-flex h-6 w-11 shrink-0 items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
->>>>>>> upstream/next-tailwind-4
 		className
 	)}
 	{...restProps}
