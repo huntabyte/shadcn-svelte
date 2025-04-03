567f6884a67e8f45d840adcf16aed577eb14e3c6
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-04-03 11:37:41 +0200

    chore: more tv4 styles

--- sites/docs/src/lib/registry/ui/popover/index.ts
+++ sites/docs/src/lib/registry/ui/popover/index.ts
@@ -1,7 +1,7 @@
 import { Popover as PopoverPrimitive } from "bits-ui";
 import Content from "./popover-content.svelte";
+import Trigger from "./popover-trigger.svelte";
 const Root = PopoverPrimitive.Root;
-const Trigger = PopoverPrimitive.Trigger;
 const Close = PopoverPrimitive.Close;
 
 export {
