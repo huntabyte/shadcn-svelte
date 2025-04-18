8b175d500d95af4e0140aff3c4e4e2cfd6dc3020
Aidan Bleser <aidanbleser35@gmail.com>
2025-04-18 06:10:42 -0500

    try this instead

--- sites/docs/src/lib/components/docs/nav/mobile-nav.svelte
+++ sites/docs/src/lib/components/docs/nav/mobile-nav.svelte
@@ -56,7 +56,7 @@
 			<Icon.Logo class="mr-2 size-4" />
 			<span class="font-bold">{siteConfig.name}</span>
 		</MobileLink>
-		<ScrollArea orientation="both" class="h-[calc(100vh-4.5rem)] py-4 pl-6">
+		<div class="h-[calc(100vh-4.5rem)] py-4 pl-6 overflow-y-auto">
 			<div class="flex flex-col space-y-3">
 				{#each docsConfig.mainNav as navItem, index (navItem + index.toString())}
 					{#if navItem.href}
@@ -89,6 +89,40 @@
 					</div>
 				{/each}
 			</div>
-		</ScrollArea>
+		</div>
+		<!-- <ScrollArea orientation="both" class="h-[calc(100vh-4.5rem)] py-4 pl-6">
+			<div class="flex flex-col space-y-3">
+				{#each docsConfig.mainNav as navItem, index (navItem + index.toString())}
+					{#if navItem.href}
+						<MobileLink href={navItem.href} bind:open class="text-foreground">
+							{navItem.title}
+						</MobileLink>
+					{/if}
+				{/each}
+			</div>
+			<div class="flex flex-col space-y-2">
+				{#each docsConfig.sidebarNav as navItem, index (index)}
+					<div class="flex flex-col space-y-3 pt-6">
+						<h4 class="font-medium">{navItem.title}</h4>
+						{#if navItem?.items?.length}
+							{#each navItem.items as item}
+								{#if !item.disabled && item.href}
+									<MobileLink href={item.href} bind:open>
+										{item.title}
+										{#if item.label}
+											<span
+												class="ml-2 rounded-md bg-[#adfa1d] px-1.5 py-0.5 text-xs leading-none text-[#000000] no-underline group-hover:no-underline"
+											>
+												{item.label}
+											</span>
+										{/if}
+									</MobileLink>
+								{/if}
+							{/each}
+						{/if}
+					</div>
+				{/each}
+			</div>
+		</ScrollArea> -->
 	</Sheet.Content>
 </Sheet.Root>
