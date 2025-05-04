8034853da873348f65667f70c8aac79bdf113340
Hunter Johnston <johnstonhuntera@gmail.com>
2025-05-03 20:42:55 -0400

    save

--- v4/src/lib/components/team-switcher.svelte
+++ v4/src/lib/components/team-switcher.svelte
@@ -37,7 +37,7 @@
 				{/snippet}
 			</DropdownMenu.Trigger>
 			<DropdownMenu.Content
-				class="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+				class="w-(--bits-dropdown-menu-anchor-width) min-w-56 rounded-lg"
 				align="start"
 				side={sidebar.isMobile ? "bottom" : "right"}
 				sideOffset={4}
