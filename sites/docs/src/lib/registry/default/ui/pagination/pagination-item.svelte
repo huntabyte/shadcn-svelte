aa6a25a2a308a86c730a5e33e183cab27ee619d5
Henrique Ramos <henriquealberone@outlook.com>
2025-05-12 16:16:25 -0300

    fix: add isVisible prop to new-york UI pagination-item

--- sites/docs/src/lib/registry/default/ui/pagination/pagination-item.svelte
+++ sites/docs/src/lib/registry/default/ui/pagination/pagination-item.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import type { HTMLLiAttributes } from "svelte/elements";
 	import type { WithElementRef } from "bits-ui";
-  import { cn } from "$lib/utils.js";
+	import { cn } from "$lib/utils.js";
 
 	let {
 		ref = $bindable(null),
 		children,
-    isVisible = true,
-    class: className,
+		isVisible = true,
+		class: className,
 		...restProps
-	}: {isVisible?: boolean } & WithElementRef<HTMLLiAttributes> = $props();
+	}: { isVisible?: boolean } & WithElementRef<HTMLLiAttributes> = $props();
 </script>
 
-<li bind:this={ref} class={cn(className, { 'hidden': !isVisible })} {...restProps}>
+<li bind:this={ref} class={cn(className, { hidden: !isVisible })} {...restProps}>
 	{@render children?.()}
 </li>
