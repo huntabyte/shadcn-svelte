c5fec2d465d50744e05934d33646f539150291b3
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-05-14 14:17:44 +0200

    more

--- v4/src/lib/components/list-item.svelte
+++ v4/src/lib/components/list-item.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
 	import * as NavigationMenu from "$lib/registry/ui/navigation-menu/index.js";
-	import type { HTMLLiAttributes } from "svelte/elements";
+	import { cn } from "$lib/utils";
+	import type { WithElementRef } from "bits-ui";
+	import type { HTMLAnchorAttributes } from "svelte/elements";
 
-	let { title, children, href, ...restProps }: HTMLLiAttributes & { href: string } = $props();
+	let {
+		ref = $bindable(null),
+		title,
+		children,
+		href,
+		class: className,
+		...restProps
+	}: WithElementRef<HTMLAnchorAttributes & { href: string }> = $props();
 </script>
 
-<li {...restProps}>
-	<NavigationMenu.Link {href}>
-		<div class="text-sm font-medium leading-none">{title}</div>
-		<p class="text-muted-foreground line-clamp-2 text-sm leading-snug">
-			{@render children?.()}
-		</p>
+<li>
+	<NavigationMenu.Link>
+		{#snippet child()}
+			<a
+				{href}
+				bind:this={ref}
+				class={cn(
+					"hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors",
+					className
+				)}
+				{...restProps}
+			>
+				<div class="text-sm font-medium leading-none">{title}</div>
+				<p class="text-muted-foreground line-clamp-2 text-sm leading-snug">
+					{@render children?.()}
+				</p>
+			</a>
+		{/snippet}
 	</NavigationMenu.Link>
 </li>
