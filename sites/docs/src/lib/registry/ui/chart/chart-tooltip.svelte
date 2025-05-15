eedd2e796a302b5458b9eb358981868c61977df7
Hunter Johnston <johnstonhuntera@gmail.com>
2025-05-14 22:13:56 -0400

    tweaks

--- sites/docs/src/lib/registry/ui/chart/chart-tooltip.svelte
+++ sites/docs/src/lib/registry/ui/chart/chart-tooltip.svelte
@@ -87,7 +87,7 @@
 <TooltipPrimitive.Root variant="none">
 	<div
 		class={cn(
-			"border-border/50 bg-background grid min-w-[8rem] items-start gap-1.5 rounded-lg border px-2.5 py-1.5 text-xs shadow-xl",
+			"border-border/50 bg-background grid min-w-[9rem] items-start gap-1.5 rounded-lg border px-2.5 py-1.5 text-xs shadow-xl",
 			className
 		)}
 		{...restProps}
@@ -134,7 +134,7 @@
 						{/if}
 						<div
 							class={cn(
-								"flex flex-1 justify-between leading-none",
+								"flex flex-1 shrink-0 justify-between leading-none",
 								nestLabel ? "items-end" : "items-center"
 							)}
 						>
