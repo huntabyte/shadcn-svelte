2ca90869c12cbc29a6d0abead9e4c7cc38d2a951
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2025-05-16 21:33:03 -0400

    tw4: registry template (#1911)

---
+++ registry-template/src/lib/registry/ui/card/index.ts
@@ -0,0 +1,24 @@
+import Card from "./card.svelte";
+import CardHeader from "./card-header.svelte";
+import CardTitle from "./card-title.svelte";
+import CardDescription from "./card-description.svelte";
+import CardAction from "./card-action.svelte";
+import CardFooter from "./card-footer.svelte";
+import CardContent from "./card-content.svelte";
+
+export {
+	Card,
+	CardHeader,
+	CardTitle,
+	CardDescription,
+	CardAction,
+	CardFooter,
+	CardContent,
+	Card as Root,
+	CardHeader as Header,
+	CardTitle as Title,
+	CardDescription as Description,
+	CardAction as Action,
+	CardFooter as Footer,
+	CardContent as Content,
+};
---
+++ registry-template/src/lib/registry/ui/input/index.ts
@@ -0,0 +1,3 @@
+import Input from "./input.svelte";
+
+export { Input, Input as Root };
