799b714b50db935ac49ee6d5582f9c08c3e69c51
Hunter Johnston <johnstonhuntera@gmail.com>
2025-05-17 23:43:28 -0400

    we really like removing stuff

--- sites/docs/src/lib/utils.ts
+++ sites/docs/src/lib/utils.ts
@@ -1,9 +1,7 @@
 import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
-import { derived, get, writable } from "svelte/store";
 import { twMerge } from "tailwind-merge";
 import { error } from "@sveltejs/kit";
-import { persisted } from "svelte-local-storage-store";
 import type { HTMLAnchorAttributes, HTMLAttributes, HTMLImgAttributes } from "svelte/elements";
 import type { DocResolver } from "$lib/types/docs.js";
 import { docs } from "$content/index.js";
@@ -23,97 +21,6 @@ export const isBrowser = typeof document !== "undefined";
 
 export function slugFromPath(path: string) {
 	return path.replace("/src/content/", "").replace(".md", "");
-}
-
-export function hexToHsl(hex: string): [number, number, number] {
-	if (hex) {
-		const sanitizedHex = hex.replace("#", "");
-
-		const red = Number.parseInt(sanitizedHex.substring(0, 2), 16);
-		const green = Number.parseInt(sanitizedHex.substring(2, 4), 16);
-		const blue = Number.parseInt(sanitizedHex.substring(4, 6), 16);
-
-		const normalizedRed = red / 255;
-		const normalizedGreen = green / 255;
-		const normalizedBlue = blue / 255;
-
-		const max = Math.max(normalizedRed, normalizedGreen, normalizedBlue);
-		const min = Math.min(normalizedRed, normalizedGreen, normalizedBlue);
-
-		let hue, saturation, lightness;
-
-		if (max === min) {
-			hue = 0;
-		} else if (max === normalizedRed) {
-			hue = ((normalizedGreen - normalizedBlue) / (max - min)) % 6;
-		} else if (max === normalizedGreen) {
-			hue = (normalizedBlue - normalizedRed) / (max - min) + 2;
-		} else {
-			hue = (normalizedRed - normalizedGreen) / (max - min) + 4;
-		}
-
-		hue = Math.round(hue * 60);
-
-		if (hue < 0) {
-			hue += 360;
-		}
-
-		lightness = (max + min) / 2;
-
-		if (max === min) {
-			saturation = 0;
-		} else if (lightness <= 0.5) {
-			saturation = (max - min) / (max + min);
-		} else {
-			saturation = (max - min) / (2 - max - min);
-		}
-
-		saturation = Math.round(saturation * 100);
-		lightness = Math.round(lightness * 100);
-
-		return [hue, saturation, lightness];
-	}
-	return [0, 0, 0];
-}
-
-export function hexToRgb(hex: string): [number, number, number] {
-	if (hex) {
-		const sanitizedHex = hex.replace("#", "");
-
-		const red = Number.parseInt(sanitizedHex.substring(0, 2), 16);
-		const green = Number.parseInt(sanitizedHex.substring(2, 4), 16);
-		const blue = Number.parseInt(sanitizedHex.substring(4, 6), 16);
-
-		return [red, green, blue];
-	}
-	return [0, 0, 0];
-}
-
-export function createCopyCodeButton() {
-	const codeString = writable("");
-	const copied = writable(false);
-	let copyTimeout = 0;
-
-	function copyCode() {
-		if (!isBrowser) return;
-		navigator.clipboard.writeText(get(codeString));
-		copied.set(true);
-		clearTimeout(copyTimeout);
-		copyTimeout = window.setTimeout(() => {
-			copied.set(false);
-		}, 2500);
-	}
-
-	function setCodeString(node: HTMLElement) {
-		codeString.set(node.innerText.trim() ?? "");
-	}
-
-	return {
-		copied,
-		copyCode,
-		setCodeString,
-		codeString,
-	};
 }
 
 export function updateTheme(activeTheme: string, path: string) {
@@ -180,23 +87,6 @@ export async function getDoc(slug: string) {
 
 export function slugFromPathname(pathname: string) {
 	return pathname.split("/").pop() ?? "";
-}
-
-const liftMode = persisted<string[]>("lift-mode", []);
-
-export function getLiftMode(name: string) {
-	function toggleLiftMode(name: string) {
-		liftMode.update((prev) => {
-			return prev.includes(name) ? prev.filter((n) => n !== name) : [...prev, name];
-		});
-	}
-
-	const isLiftMode = derived(liftMode, ($configStore) => $configStore.includes(name));
-
-	return {
-		isLiftMode,
-		toggleLiftMode,
-	};
 }
 
 // Wrappers around svelte's `HTMLAttributes` types to add a `ref` prop can be bound to
