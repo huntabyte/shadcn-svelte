2e8823b1e9d2b05cf66a5c7f5fd74d08be0c7e8a
Aidan Bleser <aidanbleser35@gmail.com>
2025-05-20 07:29:14 -0500

    fix scroll button slots

--- sites/docs/src/lib/registry/ui/select/select-scroll-down-button.svelte
+++ sites/docs/src/lib/registry/ui/select/select-scroll-down-button.svelte
@@ -12,7 +12,7 @@
 
 <SelectPrimitive.ScrollDownButton
 	bind:ref
-	data-slot="select-scroll-up-button"
+	data-slot="select-scroll-down-button"
 	class={cn("flex cursor-default items-center justify-center py-1", className)}
 	{...restProps}
 >
