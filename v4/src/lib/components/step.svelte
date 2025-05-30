cf74384c89b7f35ff955b7948588afe2ed1a07e3
Hunter Johnston <johnstonhuntera@gmail.com>
2025-05-30 14:25:26 -0400

    smore

---
+++ v4/src/lib/components/step.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { cn } from "$lib/utils.js";
+	import type { HTMLAttributes } from "svelte/elements";
+
+	let { children, class: className, ...props }: HTMLAttributes<HTMLElement> = $props();
+</script>
+
+<h3
+	class={cn("font-heading mt-8 scroll-m-32 text-xl font-medium tracking-tight", className)}
+	{...props}
+>
+	{@render children?.()}
+</h3>
---
+++ v4/src/lib/components/steps.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { cn } from "$lib/utils.js";
+	import type { HTMLAttributes } from "svelte/elements";
+
+	let { children, class: className, ...restProps }: HTMLAttributes<HTMLElement> = $props();
+</script>
+
+<div
+	class={cn("[&>h3]:step steps *:[h3]:first:!mt-0 mb-12 [counter-reset:step]", className)}
+	{...restProps}
+>
+	{@render children?.()}
+</div>
