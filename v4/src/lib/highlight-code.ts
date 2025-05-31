4f8f4e20a67a17c619a6cc9acb1956db28d37951
Hunter Johnston <johnstonhuntera@gmail.com>
2025-05-31 11:46:08 -0400

    get blocks rendering

---
+++ v4/src/lib/highlight-code.ts
@@ -0,0 +1,34 @@
+import { codeToHtml } from "shiki";
+
+const highlightCodeCache = new Map<string, string>();
+
+export async function highlightCode(code: string, language: string = "svelte") {
+	const cachedCode = highlightCodeCache.get(code);
+	if (cachedCode) return cachedCode;
+
+	const html = await codeToHtml(code, {
+		lang: language,
+		themes: {
+			dark: "github-dark",
+			light: "github-light",
+		},
+		transformers: [
+			{
+				pre(node) {
+					node.properties["class"] =
+						"no-scrollbar min-w-0 overflow-x-auto px-4 py-3.5 outline-none has-[[data-highlighted-line]]:px-0 has-[[data-line-numbers]]:px-0 has-[[data-slot=tabs]]:p-0 !bg-transparent";
+				},
+				code(node) {
+					node.properties["data-line-numbers"] = "";
+				},
+				line(node) {
+					node.properties["data-line"] = "";
+				},
+			},
+		],
+	});
+
+	highlightCodeCache.set(code, html);
+
+	return html;
+}
---
+++ v4/src/routes/(app)/blocks/+layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Announcement from "$lib/components/announcement.svelte";
+	import { Button } from "$lib/registry/ui/button/index.js";
+
+	import BlocksNav from "$lib/components/blocks-nav.svelte";
+	import PageHeader from "$lib/components/page-header/page-header.svelte";
+	import PageHeaderHeading from "$lib/components/page-header/page-header-heading.svelte";
+	import PageHeaderDescription from "$lib/components/page-header/page-header-description.svelte";
+	import PageActions from "$lib/components/page-header/page-actions.svelte";
+
+	let { children } = $props();
+
+	const title = "Building Blocks for the Web";
+	const description =
+		"Clean, modern building blocks. Copy and paste into your apps. Open Source. Free forever.";
+</script>
+
+<PageHeader>
+	<Announcement />
+	<PageHeaderHeading>{title}</PageHeaderHeading>
+	<PageHeaderDescription>{description}</PageHeaderDescription>
+	<PageActions>
+		<Button href="#blocks" size="sm">Browse Blocks</Button>
+		<Button href="/docs/blocks" variant="ghost" size="sm">Add a block</Button>
+	</PageActions>
+</PageHeader>
+<div id="blocks" class="border-grid scroll-mt-24 border-b">
+	<div class="container-wrapper">
+		<div class="container flex items-center py-4">
+			<BlocksNav />
+		</div>
+	</div>
+</div>
+<div class="container-wrapper flex-1">{@render children()}</div>
