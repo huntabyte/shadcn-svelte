41222b96e7a16f1221fcf81bf3dfd9f8a9f1e652
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-06-03 18:07:04 -0400

    update svelte, kit, and vite-plugin

--- v4/src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import { dev } from "$app/environment";
-import type { Handle } from "@sveltejs/kit";
-
-export const handle: Handle = async ({ event, resolve }) => {
-	if (dev && event.url.pathname === "/.well-known/appspecific/com.chrome.devtools.json") {
-		return new Response(undefined, { status: 404 });
-	}
-	return resolve(event);
-};
