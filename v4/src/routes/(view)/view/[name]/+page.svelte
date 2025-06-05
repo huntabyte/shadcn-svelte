4722b20c183a2cf62c14b3bdc0284dda2c3f085a
Hunter Johnston <johnstonhuntera@gmail.com>
2025-06-04 20:55:32 -0700

    lets give this a shot

--- v4/src/routes/(view)/view/[name]/+page.svelte
+++ v4/src/routes/(view)/view/[name]/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import { cn } from "$lib/utils.js";
-	import { Blocks } from "$lib/../__registry__/blocks.js";
 	import type { PageProps } from "./$types.js";
 
 	let { data }: PageProps = $props();
-
-	const block = $derived(Blocks[data.block.name]);
 </script>
 
-<div class={cn("bg-background", data.block?.container?.className)}>
-	{#await block.component() then BlockComponent}
-		<BlockComponent />
-	{/await}
+<div class={cn("bg-background")}>
+	<data.component />
 </div>
