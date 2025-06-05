4636152b3a94754223ed84e4021aaa9860118aa0
Roger Versluis <roger.versluis@fieldcap.ca>
2025-06-05 09:23:42 -0600

    Add Svelte5 repo link to the bug_report.yml.

---
+++ sites/docs/src/routes/(app)/repro5/+page.ts
@@ -0,0 +1,6 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types.js";
+
+export const load: PageLoad = async () => {
+	redirect(302, "https://stackblitz.com/github/huntabyte/shadcn-repro-template/tree/next");
+};
