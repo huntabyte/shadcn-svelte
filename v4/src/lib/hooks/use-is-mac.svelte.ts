178dc0f8cd6f324a661e32a26f1f07f792b66ff7
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-06-06 00:29:29 -0400

    fix macky mac

--- v4/src/lib/hooks/use-is-mac.svelte.ts
+++ v4/src/lib/hooks/use-is-mac.svelte.ts
@@ -1,5 +1,7 @@
+import { browser } from "$app/environment";
+
 export function useIsMac(): { readonly current: boolean } {
-	const isMac = $derived(navigator.platform.includes("MAC"));
+	const isMac = $derived(browser ? navigator.platform.includes("MAC") : false);
 
 	return {
 		get current(): boolean {
