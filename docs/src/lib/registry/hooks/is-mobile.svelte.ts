2dc65cf02452cf56aff387fe7a4a40c309b583fb
Hunter Johnston <johnstonhuntera@gmail.com>
2025-06-07 22:20:16 -0400

    extend ismobile

--- docs/src/lib/registry/hooks/is-mobile.svelte.ts
+++ docs/src/lib/registry/hooks/is-mobile.svelte.ts
@@ -1,9 +1,9 @@
 import { MediaQuery } from "svelte/reactivity";
 
-const MOBILE_BREAKPOINT = 768;
+const DEFAULT_MOBILE_BREAKPOINT = 768;
 
 export class IsMobile extends MediaQuery {
-	constructor() {
-		super(`max-width: ${MOBILE_BREAKPOINT - 1}px`);
+	constructor(breakpoint: number = DEFAULT_MOBILE_BREAKPOINT) {
+		super(`max-width: ${breakpoint - 1}px`);
 	}
 }
