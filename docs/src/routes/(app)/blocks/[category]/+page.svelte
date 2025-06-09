9f9e098105de22f2d0c309543c815e5f3d6ae55f
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-06-09 16:42:49 -0400

    would you work

--- docs/src/routes/(app)/blocks/[category]/+page.svelte
+++ docs/src/routes/(app)/blocks/[category]/+page.svelte
@@ -3,8 +3,11 @@
 	import BlockViewer from "$lib/components/block-viewer.svelte";
 	import { createFileTreeForRegistryItemFiles } from "$lib/registry/registry-utils.js";
 	import ComponentPreview from "$lib/components/component-preview.svelte";
+	import { IsMobile } from "$lib/registry/hooks/is-mobile.svelte.js";
 
 	let { data }: { data: PageData } = $props();
+
+	const mobile = new IsMobile(1024);
 </script>
 
 <div class="flex flex-col gap-12 md:gap-24">
@@ -14,16 +17,18 @@
 			tree={createFileTreeForRegistryItemFiles(block.files)}
 			highlightedFiles={block.files}
 		>
-			{#await block.component}
-				loading
-			{:then component}
-				<ComponentPreview
-					name={block.name}
-					{component}
-					hideCode
-					class="**:[.preview]:h-auto **:[.preview]:p-4 **:[.preview>.p-6]:p-0 my-0"
-				/>
-			{/await}
+			{#if mobile.current}
+				{#await block.component}
+					loading
+				{:then component}
+					<ComponentPreview
+						name={block.name}
+						{component}
+						hideCode
+						class="**:[.preview]:h-auto **:[.preview]:p-4 **:[.preview>.p-6]:p-0 my-0"
+					/>
+				{/await}
+			{/if}
 		</BlockViewer>
 	{/each}
 </div>
