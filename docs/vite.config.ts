25e90e50fb1b2f48ef55068f0e138b6d7b1bc201
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-06-10 11:54:07 -0400

    for the love of god

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -51,16 +51,14 @@ export default defineConfig({
 	},
 	build: {
 		rollupOptions: {
-			// prevents `clsx` from being built as a side-effect
-			treeshake: { moduleSideEffects: "no-external" },
 			output: {
 				manualChunks: {
 					icons: ["@lucide/svelte", "@tabler/icons-svelte"],
 				},
 			},
-			external: [/^node:.*/],
 		},
 	},
+	resolve: { external: ["node:dns"] },
 });
 
 function writeJsonSchemas() {
