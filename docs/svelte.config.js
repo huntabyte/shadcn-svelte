b920349851dcea4dab8266dca182296d0a5e7dd8
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-06-10 11:15:26 -0400

    shouldnt need this any more

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -11,37 +11,7 @@ const config = {
 
 	kit: {
 		// https://kit.svelte.dev/docs/adapter-cloudflare#options
-		adapter: adapter({
-			routes: {
-				// Since we have so many static assets, we'll manually define
-				// the globs for them to save our 100 include/exclude limit
-				exclude: [
-					"<build>",
-					// pre-rendered content
-					"/docs/*",
-					"/docs.html",
-					"/blocks/*",
-					"/blocks.html",
-					"/view/*",
-					// static
-					"/registry/*",
-					"/fonts/*",
-					"/avatars/*",
-					"/img/*",
-					"/schema/*",
-					"/android-chrome-192x192.png",
-					"/android-chrome-512x512.png",
-					"/apple-touch-icon.png",
-					"/favicon-16x16.png",
-					"/favicon-32x32.png",
-					"/favicon.ico",
-					"/og.png",
-					"/schema.json",
-					"/site.webmanifest",
-					"/themes.css",
-				],
-			},
-		}),
+		adapter: adapter(),
 		prerender: {
 			handleMissingId: (details) => {
 				if (details.id === "#") return;
