847daf8e4a84a01bf3466392f14550640a1276ff
Tom <github@thunze.de>
2025-06-26 16:08:42 +0200

    fix: tooltip arrow alignment when side is bottom or left (#2176)

--- docs/src/lib/registry/ui/tooltip/tooltip-content.svelte
+++ docs/src/lib/registry/ui/tooltip/tooltip-content.svelte
@@ -34,9 +34,9 @@
 					class={cn(
 						"bg-primary z-50 size-2.5 rotate-45 rounded-[2px]",
 						"data-[side=top]:translate-x-1/2 data-[side=top]:translate-y-[calc(-50%_+_2px)]",
-						"data-[side=bottom]:-translate-y-[calc(-50%_+_1px)] data-[side=bottom]:translate-x-1/2",
+						"data-[side=bottom]:-translate-x-1/2 data-[side=bottom]:-translate-y-[calc(-50%_+_1px)]",
 						"data-[side=right]:translate-x-[calc(50%_+_2px)] data-[side=right]:translate-y-1/2",
-						"data-[side=left]:translate-y-[calc(50%_-_3px)]",
+						"data-[side=left]:-translate-y-[calc(50%_-_3px)]",
 						arrowClasses
 					)}
 					{...props}
