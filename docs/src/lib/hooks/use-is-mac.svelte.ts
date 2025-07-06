49b5448290ba1c9b2c6950999672d3c6487b3c1f
milksense <29128703+milksense@users.noreply.github.com>
2025-07-06 07:14:38 +0300

    fix: hook use-is-mac

--- docs/src/lib/hooks/use-is-mac.svelte.ts
+++ docs/src/lib/hooks/use-is-mac.svelte.ts
@@ -1,7 +1,7 @@
 import { browser } from "$app/environment";
 
 export function useIsMac(): { readonly current: boolean } {
-	const isMac = $derived(browser ? navigator.platform.includes("MAC") : false);
+	const isMac = $derived(browser ? navigator.platform.includes("Mac") : false);
 
 	return {
 		get current(): boolean {
