fe30fc29c6f06d0ba4ec3b226d47a4461fc87cec
Max Farrell <maxffarrell@gmail.com>
2025-11-22 20:41:06 -0500

    dark mode errors page

---
+++ docs/src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { ModeWatcher } from "mode-watcher";
+</script>
+
+<ModeWatcher />
+
+<h1>{$page.status}</h1>
+<p>{$page.error?.message}</p>
