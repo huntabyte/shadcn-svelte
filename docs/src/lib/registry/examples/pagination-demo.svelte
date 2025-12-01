cf8eb764bbad50812b5f56293dc8d1ca707d54a8
Max Farrell <maxffarrell@gmail.com>
2025-11-30 22:12:12 -0500

    pagination

--- docs/src/lib/registry/examples/pagination-demo.svelte
+++ docs/src/lib/registry/examples/pagination-demo.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
-	import ChevronLeftIcon from "@lucide/svelte/icons/chevron-left";
-	import ChevronRightIcon from "@lucide/svelte/icons/chevron-right";
-	import { MediaQuery } from "svelte/reactivity";
 	import * as Pagination from "$lib/registry/ui/pagination/index.js";
-
-	const isDesktop = new MediaQuery("(min-width: 768px)");
-
-	const count = 20;
-
-	const perPage = $derived(isDesktop.current ? 3 : 8);
-	const siblingCount = $derived(isDesktop.current ? 1 : 0);
 </script>
 
-<Pagination.Root {count} {perPage} {siblingCount}>
-	{#snippet children({ pages, currentPage })}
+<Pagination.Root count={30} page={2}>
+	{#snippet children({ pages })}
 		<Pagination.Content>
 			<Pagination.Item>
-				<Pagination.PrevButton>
-					<ChevronLeftIcon class="size-4" />
-					<span class="hidden sm:block">Previous</span>
-				</Pagination.PrevButton>
+				<Pagination.Previous />
 			</Pagination.Item>
 			{#each pages as page (page.key)}
 				{#if page.type === "ellipsis"}
@@ -28,17 +15,17 @@
 					</Pagination.Item>
 				{:else}
 					<Pagination.Item>
-						<Pagination.Link {page} isActive={currentPage === page.value}>
+						<Pagination.Link {page} isActive={2 === page.value}>
 							{page.value}
 						</Pagination.Link>
 					</Pagination.Item>
 				{/if}
 			{/each}
 			<Pagination.Item>
-				<Pagination.NextButton>
-					<span class="hidden sm:block">Next</span>
-					<ChevronRightIcon class="size-4" />
-				</Pagination.NextButton>
+				<Pagination.Ellipsis />
+			</Pagination.Item>
+			<Pagination.Item>
+				<Pagination.Next />
 			</Pagination.Item>
 		</Pagination.Content>
 	{/snippet}
