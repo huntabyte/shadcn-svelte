240b91ddbd817e4abc389243ef96e6a2cf6e774f
Hunter Johnston <johnstonhuntera@gmail.com>
2025-12-08 21:26:03 -0500

    formatting

--- docs/src/lib/registry/examples/dark-mode-light-switch.svelte
+++ docs/src/lib/registry/examples/dark-mode-light-switch.svelte
@@ -8,10 +8,10 @@
 
 <Button onclick={toggleMode} variant="outline" size="icon">
 	<SunIcon
-		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all! dark:-rotate-90 dark:scale-0"
+		class="transition-all! h-[1.2rem] w-[1.2rem] rotate-0 scale-100 dark:-rotate-90 dark:scale-0"
 	/>
 	<MoonIcon
-		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all! dark:rotate-0 dark:scale-100"
+		class="transition-all! absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100"
 	/>
 	<span class="sr-only">Toggle theme</span>
 </Button>
