cfd8c52642f62156de37c31b4d928490a7553f86
Jonathan Gamble <jdgamble555@gmail.com>
2025-12-13 20:24:09 -0600

    format

--- docs/src/lib/registry/ui/toggle-group/toggle-group-item.svelte
+++ docs/src/lib/registry/ui/toggle-group/toggle-group-item.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { ToggleGroup as ToggleGroupPrimitive } from 'bits-ui';
-	import { getToggleGroupCtx } from './toggle-group.svelte';
-	import { cn } from '$lib/utils.js';
-	import { type ToggleVariants, toggleVariants } from '$lib/components/ui/toggle/index.js';
+	import { ToggleGroup as ToggleGroupPrimitive } from "bits-ui";
+	import { getToggleGroupCtx } from "./toggle-group.svelte";
+	import { cn } from "$lib/utils.js";
+	import { type ToggleVariants, toggleVariants } from "$lib/components/ui/toggle/index.js";
 
 	let {
 		ref = $bindable(null),
@@ -24,9 +24,9 @@
 	class={cn(
 		toggleVariants({
 			variant: ctx().variant || variant,
-			size: ctx().size || size
+			size: ctx().size || size,
 		}),
-		'min-w-0 flex-1 shrink-0 rounded-none shadow-none first:rounded-s-md last:rounded-e-md focus:z-10 focus-visible:z-10 data-[variant=outline]:border-s-0 data-[variant=outline]:first:border-s',
+		"min-w-0 flex-1 shrink-0 rounded-none shadow-none first:rounded-s-md last:rounded-e-md focus:z-10 focus-visible:z-10 data-[variant=outline]:border-s-0 data-[variant=outline]:first:border-s",
 		className
 	)}
 	{value}
--- docs/src/lib/registry/ui/toggle-group/toggle-group.svelte
+++ docs/src/lib/registry/ui/toggle-group/toggle-group.svelte
@@ -1,34 +1,34 @@
 <script lang="ts" module>
-	import { getContext, setContext } from 'svelte';
-	import { type ToggleVariants } from '$lib/components/ui/toggle/index.js';
+	import { getContext, setContext } from "svelte";
+	import { type ToggleVariants } from "$lib/components/ui/toggle/index.js";
 
 	export type ToggleVariantsSignal = () => ToggleVariants;
 
 	export function setToggleGroupCtx(props: ToggleVariantsSignal) {
-		setContext('toggleGroup', props);
+		setContext("toggleGroup", props);
 	}
 
 	export function getToggleGroupCtx() {
-		return getContext<ToggleVariantsSignal>('toggleGroup');
+		return getContext<ToggleVariantsSignal>("toggleGroup");
 	}
 </script>
 
 <script lang="ts">
-	import { ToggleGroup as ToggleGroupPrimitive } from 'bits-ui';
-	import { cn } from '$lib/utils.js';
+	import { ToggleGroup as ToggleGroupPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils.js";
 
 	let {
 		ref = $bindable(null),
 		value = $bindable(),
 		class: className,
-		size = 'default',
-		variant = 'default',
+		size = "default",
+		variant = "default",
 		...restProps
 	}: ToggleGroupPrimitive.RootProps & ToggleVariants = $props();
 
 	const toggleCtx = $derived({
 		variant,
-		size
+		size,
 	});
 
 	setToggleGroupCtx(() => toggleCtx);
@@ -45,7 +45,7 @@ get along, so we shut typescript up by casting `value` to `never`.
 	data-variant={variant}
 	data-size={size}
 	class={cn(
-		'group/toggle-group flex w-fit items-center rounded-md data-[variant=outline]:shadow-xs',
+		"group/toggle-group data-[variant=outline]:shadow-xs flex w-fit items-center rounded-md",
 		className
 	)}
 	{...restProps}
