3f6ccd5e5ce4e4ceb73be8fdf26076c7b8611c1a
Aidan Bleser <aidanbleser35@gmail.com>
2025-12-17 13:35:52 -0600

    make it better

--- docs/src/routes/(app)/(layout)/(create)/components/radius-picker.svelte
+++ docs/src/routes/(app)/(layout)/(create)/components/radius-picker.svelte
@@ -7,7 +7,9 @@
 
 	const params = useCreateSearchParams();
 
-	const currentRadius = $derived(RADII.find((radius) => radius.name === params.radius) ?? RADII[0]);
+	const currentRadius = $derived(
+		RADII.find((radius) => radius.name === params.radius) ?? RADII[0]
+	);
 
 	const isMobile = new IsMobile();
 </script>
@@ -22,24 +24,12 @@
 				</div>
 			</div>
 			<div
-				class="text-foreground pointer-events-none absolute right-4 top-1/2 flex size-4 -translate-y-1/2 rotate-90 select-none items-center justify-center text-base"
+				class="text-foreground pointer-events-none absolute right-4 top-1/2 flex size-4 -translate-y-1/2 select-none items-center justify-center text-base"
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					class="text-foreground"
-				>
-					<path
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 20v-5C4 8.925 8.925 4 15 4h5"
-					/>
-				</svg>
+				<div
+					class="size-4 border-r-2 border-t-2 border-current transition-all"
+					style="border-top-right-radius: {currentRadius?.value};"
+				></div>
 			</div>
 		</Picker.Trigger>
 		<Picker.Content side={isMobile ? "top" : "right"} align={isMobile ? "center" : "start"}>
@@ -68,5 +58,5 @@
 			</Picker.RadioGroup>
 		</Picker.Content>
 	</Picker.Root>
-	<LockButton prop="radius" class="absolute top-1/2 right-10 -translate-y-1/2" />
+	<LockButton prop="radius" class="absolute right-10 top-1/2 -translate-y-1/2" />
 </div>
