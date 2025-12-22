4dbf715aee10deb2c1a65a375ecc8bff7016bfa5
Aidan Bleser <aidanbleser35@gmail.com>
2025-12-22 10:38:49 -0600

    make themes use the design system provider

--- docs/src/routes/(app)/+layout.svelte
+++ docs/src/routes/(app)/+layout.svelte
@@ -1,35 +1,17 @@
 <script lang="ts">
-	import { watch } from "runed";
 	import { UserConfig, UserConfigContext } from "$lib/user-config.svelte.js";
-	import { ModeWatcher, setTheme } from "mode-watcher";
+	import { ModeWatcher } from "mode-watcher";
 	import { Toaster } from "$lib/registry/ui/sonner/index.js";
 	import { DesignSystemProvider } from "$lib/features/design-system/index.js";
 
 	let { children, data } = $props();
 
-	const userConfig = UserConfigContext.set(new UserConfig(data.userConfig));
-
-	const themeColors = { light: "#ffffff", dark: "#09090b" };
-	const modeClasses = $derived([
-		`layout-${userConfig.current.layout}`,
-		`theme-${userConfig.current.activeTheme}`,
-	]);
-
-	watch.pre(
-		() => userConfig.current.activeTheme,
-		() => {
-			setTheme(userConfig.current.activeTheme);
-		}
-	);
+	UserConfigContext.set(new UserConfig(data.userConfig));
 </script>
 
 <ModeWatcher
 	defaultMode="system"
 	disableTransitions
-	defaultTheme={userConfig.current.activeTheme}
-	{themeColors}
-	darkClassNames={["dark", ...modeClasses]}
-	lightClassNames={["light", ...modeClasses]}
 />
 <Toaster position="top-center" />
 
