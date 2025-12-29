d22ec76ba4e3bb642c768a48b6c5b4a5f3b32e90
Aidan Bleser <aidanbleser35@gmail.com>
2025-12-29 08:43:12 -0600

    remove old examples

--- docs/src/lib/registry/examples/create/navigation-menu/navigation-menu.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import ExampleWrapper from "../../../../../routes/(app)/(layout)/(create)/components/example-wrapper.svelte";
-	import NavigationMenuBasic from "./navigation-menu-basic.svelte";
-</script>
-
-<ExampleWrapper class="lg:grid-cols-1">
-	<NavigationMenuBasic />
-</ExampleWrapper>
