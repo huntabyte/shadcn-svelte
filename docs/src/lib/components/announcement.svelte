d9e94353bf70b739b51eb74369a65c4a140bc203
Max Farrell <maxffarrell@gmail.com>
2026-03-12 22:35:19 -0500

    Update announcement.svelte

--- docs/src/lib/components/announcement.svelte
+++ docs/src/lib/components/announcement.svelte
@@ -4,10 +4,6 @@
 	import { ArrowRightIcon } from "@lucide/svelte";
 </script>
 
-<a
-	href="/docs/changelog"
-	class={cn(badgeVariants({ variant: "secondary", class: "bg-transparent" }))}
->
-	<span class="bg-svelte-orange size-2 rounded-full" title="New"></span>
-	New Components: Field, Input Group, Item and more <ArrowRightIcon />
+<a href="/docs/changelog" class={cn(badgeVariants({ variant: "secondary", class: "bg-muted" }))}>
+	shadcn-svelte/skills, presets and more <ArrowRightIcon />
 </a>
