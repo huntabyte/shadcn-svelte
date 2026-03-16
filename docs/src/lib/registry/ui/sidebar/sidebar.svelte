ebc2ec49a74dfe6f711e1d60e93d3e603235ed20
cleveng <cleveng@gmail.com>
2026-03-16 15:30:26 +0800

    Support class binding in sidebar.svelte on mobile
    eg: safe area inset top for mobile sidebar

--- docs/src/lib/registry/ui/sidebar/sidebar.svelte
+++ docs/src/lib/registry/ui/sidebar/sidebar.svelte
@@ -43,7 +43,7 @@
 			data-sidebar="sidebar"
 			data-slot="sidebar"
 			data-mobile="true"
-			class="bg-sidebar text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
+			class={cn("bg-sidebar text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden", className)}
 			style="--sidebar-width: {SIDEBAR_WIDTH_MOBILE};"
 			{side}
 		>
