ca4b38efc929f85463e9e1ab5577f3773852bc35
Aidan Bleser <aidanbleser35@gmail.com>
2026-03-23 15:19:19 -0500

    Update progress.svelte

--- docs/src/lib/registry/ui/progress/progress.svelte
+++ docs/src/lib/registry/ui/progress/progress.svelte
@@ -22,6 +22,6 @@
 	<div
 		data-slot="progress-indicator"
 		class="cn-progress-indicator size-full flex-1 transition-all"
-		style="transform: translateX(-{100 - (value ?? 0)}%)"
+		style="transform: translateX(-{100 - (100 * (value ?? 0)) / (max ?? 1)}%)"
 	></div>
 </ProgressPrimitive.Root>
