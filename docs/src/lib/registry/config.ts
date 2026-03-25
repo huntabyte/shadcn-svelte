9f7d39f30e06f31ef422eb72c8646b94c8fb3b59
Aidan Bleser <aidanbleser35@gmail.com>
2026-03-25 12:26:16 -0500

    fix: Ensure tailwind utilities are installed on `add`/`update`

--- docs/src/lib/registry/config.ts
+++ docs/src/lib/registry/config.ts
@@ -7,7 +7,7 @@ import { fonts } from "./fonts.js";
 import { STYLES, type Style } from "./styles/index.js";
 import { BASE_THEMES, THEMES, type BaseTheme, type Theme } from "./themes.js";
 import { PRESET_BASE_COLOR_KEYS, PRESET_FONTS, type PresetConfig } from "shadcn-svelte/preset";
-
+import { TAILWIND_UTILS } from "shadcn-svelte/utils/css";
 export { STYLES, type Style };
 export { THEMES, type Theme };
 export { BASE_THEMES, type BaseTheme };
@@ -282,66 +282,7 @@ export function buildRegistryBase(config: PresetConfig) {
 				"*": { "@apply border-border outline-ring/50": {} },
 				body: { "@apply bg-background text-foreground": {} },
 			},
-
-			// these are the styles that `@import "shadcn/tailwind.css"` adds
-			// but that seems like the wrong solution so for the time being we'll add them on init
-			"@custom-variant data-open": {
-				'&:where([data-state="open"]), &:where([data-open]:not([data-open="false"]))': {
-					"@slot": {},
-				},
-			},
-			"@custom-variant data-closed": {
-				'&:where([data-state="closed"]), &:where([data-closed]:not([data-closed="false"]))':
-					{
-						"@slot": {},
-					},
-			},
-			"@custom-variant data-checked": {
-				'&:where([data-state="checked"]), &:where([data-checked]:not([data-checked="false"]))':
-					{
-						"@slot": {},
-					},
-			},
-			"@custom-variant data-unchecked": {
-				'&:where([data-state="unchecked"]), &:where([data-unchecked]:not([data-unchecked="false"]))':
-					{
-						"@slot": {},
-					},
-			},
-			"@custom-variant data-selected": {
-				"&:where([data-selected])": {
-					"@slot": {},
-				},
-			},
-			"@custom-variant data-disabled": {
-				'&:where([data-disabled="true"]), &:where([data-disabled]:not([data-disabled="false"]))':
-					{
-						"@slot": {},
-					},
-			},
-			"@custom-variant data-active": {
-				'&:where([data-state="active"]), &:where([data-active]:not([data-active="false"]))':
-					{
-						"@slot": {},
-					},
-			},
-			"@custom-variant data-horizontal": {
-				'&:where([data-orientation="horizontal"])': {
-					"@slot": {},
-				},
-			},
-			"@custom-variant data-vertical": {
-				'&:where([data-orientation="vertical"])': {
-					"@slot": {},
-				},
-			},
-			"@utility no-scrollbar": {
-				"-ms-overflow-style": "none",
-				"scrollbar-width": "none",
-				"&::-webkit-scrollbar": {
-					display: "none",
-				},
-			},
+			...TAILWIND_UTILS
 		},
 	};
 }
--- packages/cli/src/utils/css.ts
+++ packages/cli/src/utils/css.ts
@@ -5,3 +5,67 @@ export function createGlobalCssFile(): string {
 @custom-variant dark (&:is(.dark *));
 `;
 }
+
+/**
+ * these are the styles that `@import "shadcn/tailwind.css"` adds
+ * but that seems like the wrong solution so for the time being we'll add them on init
+ */
+export const TAILWIND_UTILS = {
+	"@custom-variant data-open": {
+		'&:where([data-state="open"]), &:where([data-open]:not([data-open="false"]))': {
+			"@slot": {},
+		},
+	},
+	"@custom-variant data-closed": {
+		'&:where([data-state="closed"]), &:where([data-closed]:not([data-closed="false"]))':
+		{
+			"@slot": {},
+		},
+	},
+	"@custom-variant data-checked": {
+		'&:where([data-state="checked"]), &:where([data-checked]:not([data-checked="false"]))':
+		{
+			"@slot": {},
+		},
+	},
+	"@custom-variant data-unchecked": {
+		'&:where([data-state="unchecked"]), &:where([data-unchecked]:not([data-unchecked="false"]))':
+		{
+			"@slot": {},
+		},
+	},
+	"@custom-variant data-selected": {
+		"&:where([data-selected])": {
+			"@slot": {},
+		},
+	},
+	"@custom-variant data-disabled": {
+		'&:where([data-disabled="true"]), &:where([data-disabled]:not([data-disabled="false"]))':
+		{
+			"@slot": {},
+		},
+	},
+	"@custom-variant data-active": {
+		'&:where([data-state="active"]), &:where([data-active]:not([data-active="false"]))':
+		{
+			"@slot": {},
+		},
+	},
+	"@custom-variant data-horizontal": {
+		'&:where([data-orientation="horizontal"])': {
+			"@slot": {},
+		},
+	},
+	"@custom-variant data-vertical": {
+		'&:where([data-orientation="vertical"])': {
+			"@slot": {},
+		},
+	},
+	"@utility no-scrollbar": {
+		"-ms-overflow-style": "none",
+		"scrollbar-width": "none",
+		"&::-webkit-scrollbar": {
+			display: "none",
+		},
+	},
+}
