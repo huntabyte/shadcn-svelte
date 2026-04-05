7cbce04a81542b0fb1198a3a9116f0ee3e5522f4
Max Farrell <maxffarrell@gmail.com>
2026-04-04 19:16:53 -0500

    Update copy-button.svelte

--- docs/src/lib/components/copy-button.svelte
+++ docs/src/lib/components/copy-button.svelte
@@ -21,28 +21,21 @@
 	const rp = $derived(restProps as any);
 </script>
 
-<Tooltip.Root disableCloseOnTriggerClick>
-	<!-- eslint-disable-next-line @typescript-eslint/no-explicit-any -->
-	<Tooltip.Trigger
-		{...rp}
-		class={cn(
-			"bg-code absolute end-2 top-3 z-10 size-7 hover:opacity-100 focus-visible:opacity-100",
-			className
-		)}
-		onclick={() => clipboard.copy(text)}
-	>
-		{#snippet child({ props })}
-			<Button {...props} data-slot="copy-button" size="icon" {variant}>
-				<span class="sr-only" data-llm-ignore>Copy</span>
-				{#if clipboard.copied}
-					<CheckIcon />
-				{:else}
-					<CopyIcon />
-				{/if}
-			</Button>
-		{/snippet}
-	</Tooltip.Trigger>
-	<Tooltip.Content>
-		{clipboard.copied ? "Copied" : "Copy to Clipboard"}
-	</Tooltip.Content>
-</Tooltip.Root>
+<Button
+	data-slot="copy-button"
+	size="icon"
+	{variant}
+	onclick={() => clipboard.copy(text)}
+	class={cn(
+		"bg-code absolute top-3 right-2 z-10 size-7 hover:opacity-100 focus-visible:opacity-100"
+	)}
+	{...rp}
+	,
+>
+	<span class="sr-only" data-llm-ignore>Copy</span>
+	{#if clipboard.copied}
+		<CheckIcon />
+	{:else}
+		<CopyIcon />
+	{/if}
+</Button>
