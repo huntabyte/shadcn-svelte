0da36ab05ca4cff0c3ca2771bf95f095c51361a9
Max Farrell <maxffarrell@gmail.com>
2026-04-05 16:41:18 -0500

    fix missing data-size

--- docs/src/lib/registry/ui/button/button.svelte
+++ docs/src/lib/registry/ui/button/button.svelte
@@ -59,6 +59,7 @@
 	<a
 		bind:this={ref}
 		data-slot="button"
+		data-size={size}
 		class={cn(buttonVariants({ variant, size }), className)}
 		href={disabled ? undefined : href}
 		aria-disabled={disabled}
@@ -72,6 +73,7 @@
 	<button
 		bind:this={ref}
 		data-slot="button"
+		data-size={size}
 		class={cn(buttonVariants({ variant, size }), className)}
 		{type}
 		{disabled}
