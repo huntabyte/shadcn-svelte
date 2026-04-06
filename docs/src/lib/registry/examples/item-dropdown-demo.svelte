eb14c091a1ed02f5028dd41d5189dbaca2fcf89c
Max Farrell <maxffarrell@gmail.com>
2026-04-06 00:57:39 -0500

    item demos

--- docs/src/lib/registry/examples/item-dropdown-demo.svelte
+++ docs/src/lib/registry/examples/item-dropdown-demo.svelte
@@ -3,7 +3,7 @@
 	import * as Avatar from "$lib/registry/ui/avatar/index.js";
 	import * as DropdownMenu from "$lib/registry/ui/dropdown-menu/index.js";
 	import { Button } from "$lib/registry/ui/button/index.js";
-	import ChevronDown from "@lucide/svelte/icons/chevron-down";
+	import ChevronDownIcon from "@lucide/svelte/icons/chevron-down";
 
 	const people = [
 		{
@@ -24,32 +24,33 @@
 	];
 </script>
 
-<div class="flex min-h-64 w-full max-w-md flex-col items-center gap-6">
-	<DropdownMenu.Root>
-		<DropdownMenu.Trigger>
-			{#snippet child({ props })}
-				<Button {...props} variant="outline" size="sm" class="w-fit">
-					Select <ChevronDown />
-				</Button>
-			{/snippet}
-		</DropdownMenu.Trigger>
-		<DropdownMenu.Content class="w-72 [--radius:0.65rem]" align="end">
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger>
+		{#snippet child({ props })}
+			<Button {...props} variant="outline">
+				Select <ChevronDownIcon />
+			</Button>
+		{/snippet}
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content class="w-max" align="end">
+		<DropdownMenu.Group>
 			{#each people as person (person.username)}
-				<DropdownMenu.Item class="p-0">
-					<Item.Root size="sm" class="w-full p-2">
+				<DropdownMenu.Item textValue={person.username}>
+					<Item.Root size="xs" class="w-full p-2">
 						<Item.Media>
-							<Avatar.Root class="size-8">
+							<Avatar.Root class="size-[--spacing(6.5)]">
 								<Avatar.Image src={person.avatar} class="grayscale" />
 								<Avatar.Fallback>{person.username.charAt(0)}</Avatar.Fallback>
 							</Avatar.Root>
 						</Item.Media>
-						<Item.Content class="gap-0.5">
+						<Item.Content class="gap-0">
 							<Item.Title>{person.username}</Item.Title>
-							<Item.Description>{person.email}</Item.Description>
+							<Item.Description class="leading-none">
+								{person.email}
+							</Item.Description>
 						</Item.Content>
 					</Item.Root>
-				</DropdownMenu.Item>
-			{/each}
-		</DropdownMenu.Content>
-	</DropdownMenu.Root>
-</div>
+				</DropdownMenu.Item>{/each}
+		</DropdownMenu.Group>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
