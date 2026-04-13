c9b1a3dd5dfbebe75c1bbaaecbbf751179a355fc
Max Farrell <maxffarrell@gmail.com>
2026-04-13 00:37:36 -0500

    match shadcn

--- docs/src/lib/components/mdsx/h2.svelte
+++ docs/src/lib/components/mdsx/h2.svelte
@@ -7,7 +7,7 @@
 
 <h2
 	class={cn(
-		"font-heading [&+]*:[code]:text-xl mt-10 scroll-m-28 text-xl font-medium tracking-tight first:mt-0 lg:mt-16 [&+.steps]:!mt-0 [&+.steps>h3]:!mt-4 [&+h3]:!mt-6 [&+p]:!mt-4",
+		"[&+]*:[code]:text-xl font-heading mt-10 scroll-m-28 text-xl font-medium tracking-tight first:mt-0 lg:mt-12 [&+.steps]:mt-0! [&+.steps>h3]:mt-4! [&+h3]:mt-6! [&+p]:mt-4!",
 		className
 	)}
 	{...restProps}
