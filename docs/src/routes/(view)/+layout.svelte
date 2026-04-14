a8b51c63a67527e2259ef302a349c769a1f545bd
Aidan Bleser <aidanbleser35@gmail.com>
2026-04-14 06:10:42 -0500

    wrapper provider

--- docs/src/routes/(view)/+layout.svelte
+++ docs/src/routes/(view)/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { ModeWatcher } from "mode-watcher";
 	import { DesignSystemProvider } from "$lib/features/design-system/index.js";
+	import * as Tooltip from "$lib/registry/ui/tooltip/index.js";
 
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
-<DesignSystemProvider>
-	{@render children()}
-</DesignSystemProvider>
+<Tooltip.Provider>
+	<DesignSystemProvider>
+		{@render children()}
+	</DesignSystemProvider>
+</Tooltip.Provider>
