866c57ea0c7786045769695d9f0e0373c2f24dcb
Max Farrell <maxffarrell@gmail.com>
2026-04-13 21:59:26 -0500

    Update page-header-heading.svelte

--- docs/src/lib/components/page-header/page-header-heading.svelte
+++ docs/src/lib/components/page-header/page-header-heading.svelte
@@ -7,7 +7,7 @@
 
 <h1
 	class={cn(
-		"text-primary leading-tighter max-w-2xl text-4xl font-semibold tracking-tight text-balance lg:leading-[1.1] lg:font-semibold xl:text-5xl xl:tracking-tighter",
+		"leading-tighter text-primary max-w-3xl text-3xl font-semibold tracking-tight text-balance lg:leading-[1.1] lg:font-semibold xl:text-5xl xl:tracking-tighter",
 		className
 	)}
 	{...restProps}
