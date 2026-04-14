966d5c37bdb92ea9c0c0d6b25b27c292169f07c0
Aidan Bleser <117548273+ieedan@users.noreply.github.com>
2026-04-14 05:47:39 -0500

    Remove TooltipProvider wrapping from tooltip component

--- docs/src/lib/registry/ui/tooltip/tooltip.svelte
+++ docs/src/lib/registry/ui/tooltip/tooltip.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import { Tooltip as TooltipPrimitive } from "bits-ui";
-	import TooltipProvider from "./tooltip-provider.svelte";
 
 	let { open = $bindable(false), ...restProps }: TooltipPrimitive.RootProps = $props();
 </script>
 
-<TooltipProvider>
-	<TooltipPrimitive.Root bind:open {...restProps} />
-</TooltipProvider>
+<TooltipPrimitive.Root bind:open {...restProps} />
