9a972a3138791693723ab91c3fa549274950e7c3
Max Farrell <maxffarrell@gmail.com>
2026-04-16 18:28:04 -0500

    changelogs

--- docs/src/lib/components/image.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { cn } from "$lib/utils.js";
-	import type { HTMLImgAttributes } from "svelte/elements";
-
-	let { class: className, src, alt, height, width, ...restProps }: HTMLImgAttributes = $props();
-</script>
-
-<img class={cn("mt-6 rounded-md border", className)} {src} {alt} {height} {width} {...restProps} />
