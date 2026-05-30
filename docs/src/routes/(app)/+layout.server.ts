27fb3e50323967b850065898dd66391d45f31803
Max Farrell <maxffarrell@gmail.com>
2026-05-30 17:54:36 -0500

    Revert layout config fallback

--- docs/src/routes/(app)/+layout.server.ts
+++ docs/src/routes/(app)/+layout.server.ts
@@ -5,13 +5,8 @@ export const load: LayoutServerLoad = async ({ cookies }) => {
 	const sidebarState = cookies.get("sidebar_state") === "true" ? true : false;
 
 	const userConfigCookie = cookies.get(USER_CONFIG_COOKIE_NAME);
-	const userConfig = (() => {
-		try {
-			return userConfigSchema.parse(userConfigCookie ? JSON.parse(userConfigCookie) : {});
-		} catch {
-			return userConfigSchema.parse({});
-		}
-	})();
+	const parsedUserConfig = userConfigCookie ? JSON.parse(userConfigCookie) : {};
+	const userConfig = userConfigSchema.parse(parsedUserConfig);
 
 	return { sidebarState, userConfig };
 };
