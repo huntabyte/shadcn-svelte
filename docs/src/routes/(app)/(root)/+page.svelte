5383d9bc15339cac47c6525b4bdc8e5ed38728ba
Max Farrell <maxffarrell@gmail.com>
2026-05-30 18:07:51 -0500

    Move homepage files to root group

--- docs/src/routes/(app)/(root)/+page.svelte
+++ docs/src/routes/(app)/(root)/+page.svelte
@@ -7,7 +7,6 @@
 	import PageHeaderHeading from "$lib/components/page-header/page-header-heading.svelte";
 	import PageHeader from "$lib/components/page-header/page-header.svelte";
 	import Button from "$lib/registry/ui/button/button.svelte";
-	import { IsMobile } from "$lib/registry/hooks/is-mobile.svelte.js";
 	import Metadata from "$lib/components/metadata.svelte";
 	import { IconArrowRight } from "@tabler/icons-svelte";
 	import CardsDemo from "./cards/cards-demo.svelte";
@@ -15,8 +14,6 @@
 	const title = "The Foundation for your Design System";
 	const description =
 		"A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.";
-
-	const mobile = new IsMobile();
 </script>
 
 <Metadata {title} {description} />
@@ -35,19 +32,21 @@
 	<div class="container-wrapper flex-1 p-0">
 		<div class="container overflow-hidden md:px-0 lg:max-w-none">
 			<section class="-mx-4 w-[140vw] overflow-hidden md:hidden">
-				<enhanced:img
-					class="block dark:hidden"
-					src="../../../../static/img/registry/dashboard-01-light.png"
+				<img
+					src="/images/full-light.png"
+					width="2560"
+					height="2764"
 					alt="Dashboard"
-					fetchpriority={mobile.current ? "high" : undefined}
-					loading={mobile.current ? "eager" : "lazy"}
-				/>
-				<enhanced:img
-					class="hidden dark:block"
-					src="../../../../static/img/registry/dashboard-01-dark.png"
-					alt="Dashboard"
+					class="block h-auto w-full dark:hidden"
 					fetchpriority="high"
-					loading={mobile.current ? "eager" : "lazy"}
+				/>
+				<img
+					src="/images/full-dark.png"
+					width="2560"
+					height="2764"
+					alt="Dashboard"
+					class="hidden h-auto w-full dark:block"
+					fetchpriority="high"
 				/>
 			</section>
 			<section class="hidden md:block">
