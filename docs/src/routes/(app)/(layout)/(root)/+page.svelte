3486f5990a09c9216db28f5f6d6ad4325c28b363
Max Farrell <maxffarrell@gmail.com>
2026-05-30 22:38:16 -0500

    use enhanced:img

--- docs/src/routes/(app)/(layout)/(root)/+page.svelte
+++ docs/src/routes/(app)/(layout)/(root)/+page.svelte
@@ -10,10 +10,13 @@
 	import Metadata from "$lib/components/metadata.svelte";
 	import { IconArrowRight } from "@tabler/icons-svelte";
 	import CardsDemo from "./cards/cards-demo.svelte";
+	import { IsMobile } from "$lib/registry/hooks/is-mobile.svelte.js";
 
 	const title = "The Foundation for your Design System";
 	const description =
 		"A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.";
+
+	const mobile = new IsMobile();
 </script>
 
 <Metadata {title} {description} />
@@ -32,28 +35,32 @@
 	<div class="container-wrapper flex-1 p-0">
 		<div class="container overflow-hidden md:px-0 lg:max-w-none">
 			<section class="-mx-4 w-[140vw] overflow-hidden md:hidden">
-				<img
+				<enhanced:img
 					src="/images/full-light.png"
 					width="2560"
 					height="2764"
 					alt="Dashboard"
 					class="block h-auto w-full dark:hidden"
 					fetchpriority="high"
+					loading={mobile.current ? "eager" : "lazy"}
 				/>
-				<img
+				<enhanced:img
 					src="/images/full-dark.png"
 					width="2560"
 					height="2764"
 					alt="Dashboard"
 					class="hidden h-auto w-full dark:block"
 					fetchpriority="high"
+					loading={mobile.current ? "eager" : "lazy"}
 				/>
 			</section>
-			<section class="hidden md:block">
-				<div class="style-rhea">
-					<CardsDemo />
-				</div>
-			</section>
+			{#if !mobile.current}
+				<section class="hidden md:block">
+					<div class="style-rhea">
+						<CardsDemo />
+					</div>
+				</section>
+			{/if}
 		</div>
 	</div>
 </div>
