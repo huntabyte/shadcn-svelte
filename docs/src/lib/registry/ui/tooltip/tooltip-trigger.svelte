c42c7d206ba3f5835cb969039a9d976a7856111e
Miquel de Domingo <miquelddg@gmail.com>
2026-06-08 18:49:22 +0200

    fix(tooltip): forward root generic to type payload

--- docs/src/lib/registry/ui/tooltip/tooltip-trigger.svelte
+++ docs/src/lib/registry/ui/tooltip/tooltip-trigger.svelte
@@ -1,7 +1,7 @@
-<script lang="ts">
+<script lang="ts" generics="P = never">
 	import { Tooltip as TooltipPrimitive } from "bits-ui";
 
-	let { ref = $bindable(null), ...restProps }: TooltipPrimitive.TriggerProps = $props();
+	let { ref = $bindable(null), ...restProps }: TooltipPrimitive.TriggerProps<P> = $props();
 </script>
 
 <TooltipPrimitive.Trigger bind:ref data-slot="tooltip-trigger" {...restProps} />
