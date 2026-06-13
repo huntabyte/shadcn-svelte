6915744576e95426450bc372a1a01648adb18daf
Max Farrell <maxffarrell@gmail.com>
2026-06-13 09:08:49 -0400

    docs: align code preview styling

--- docs/src/lib/components/component-preview-tabs.svelte
+++ docs/src/lib/components/component-preview-tabs.svelte
@@ -87,7 +87,7 @@
 				{#if isMobileCodeVisible}
 					{@render children?.()}
 				{:else}
-					<div class="relative h-24">
+					<div class="relative">
 						{@render children?.()}
 						<div class="absolute inset-0 flex items-center justify-center pb-4">
 							<div
