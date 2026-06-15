4fcdf74b0d3e6e88c84fb0000ed17f21f1a53332
Miquel de Domingo <miquelddg@gmail.com>
2026-06-15 08:33:04 +0200

    fix(tooltip): rename generic to T

--- docs/src/lib/registry/ui/tooltip/tooltip-trigger.svelte
+++ docs/src/lib/registry/ui/tooltip/tooltip-trigger.svelte
@@ -1,7 +1,7 @@
-<script lang="ts" generics="P = never">
+<script lang="ts" generics="T = never">
 	import { Tooltip as TooltipPrimitive } from "bits-ui";
 
-	let { ref = $bindable(null), ...restProps }: TooltipPrimitive.TriggerProps<P> = $props();
+	let { ref = $bindable(null), ...restProps }: TooltipPrimitive.TriggerProps<T> = $props();
 </script>
 
 <TooltipPrimitive.Trigger bind:ref data-slot="tooltip-trigger" {...restProps} />
--- docs/src/lib/registry/ui/tooltip/tooltip.svelte
+++ docs/src/lib/registry/ui/tooltip/tooltip.svelte
@@ -1,7 +1,7 @@
-<script lang="ts" generics="P = never">
+<script lang="ts" generics="T = never">
 	import { Tooltip as TooltipPrimitive } from "bits-ui";
 
-	let { open = $bindable(false), ...restProps }: TooltipPrimitive.RootProps<P> = $props();
+	let { open = $bindable(false), ...restProps }: TooltipPrimitive.RootProps<T> = $props();
 </script>
 
 <TooltipPrimitive.Root bind:open {...restProps} />
