e0b6c8ee57bd9801b3f2c0aaf8fccccb834c05ec
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2026-07-29 21:58:02 +0000

    remove prettier from style puller script

--- docs/src/routes/(app)/(layout)/docs/+layout.svelte
+++ docs/src/routes/(app)/(layout)/docs/+layout.svelte
@@ -6,7 +6,6 @@
 </script>
 
 <div class="container-wrapper flex flex-1 flex-col px-2">
-	<!-- prettier-ignore -->
 	<Sidebar.Provider
 		class="min-h-min flex-1 items-start px-0 [--top-spacing:0] lg:grid lg:grid-cols-[var(--sidebar-width)_minmax(0,1fr)] lg:[--top-spacing:calc(var(--spacing)*4)] 3xl:fixed:container 3xl:fixed:px-3"
 		style="--sidebar-width: calc(var(--spacing) * 72)"
--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import path from "node:path";
 import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
 import ts from "typescript-eslint";
@@ -14,8 +13,6 @@ export default defineConfig(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs["flat/recommended"],
-	prettier,
-	...svelte.configs["flat/prettier"],
 	{
 		languageOptions: {
 			globals: {
@@ -41,7 +38,7 @@ export default defineConfig(
 		files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
 		languageOptions: {
 			parserOptions: {
-				projectService: true,
+				// projectService: true,
 				extraFileExtensions: [".svelte"],
 				parser: ts.parser,
 			},
