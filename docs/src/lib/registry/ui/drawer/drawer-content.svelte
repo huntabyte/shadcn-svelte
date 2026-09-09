bc3c2038f92aca4bd317da0e25180bdb5a4b335d
Nureddin Haji <drnhaji@gmail.com>
2026-09-09 14:16:17 +0300

    fix(drawer): restore direction layout lost when styles were synced (#2874)

--- docs/src/lib/registry/ui/drawer/drawer-content.svelte
+++ docs/src/lib/registry/ui/drawer/drawer-content.svelte
@@ -22,7 +22,14 @@
 	<DrawerPrimitive.Content
 		bind:ref
 		data-slot="drawer-content"
-		class={cn("cn-drawer-content group/drawer-content fixed z-50", className)}
+		class={cn(
+			"cn-drawer-content group/drawer-content fixed z-50",
+			"data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh]",
+			"data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh]",
+			"data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:sm:max-w-sm",
+			"data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:left-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=left]:sm:max-w-sm",
+			className
+		)}
 		{...restProps}
 	>
 		<div
